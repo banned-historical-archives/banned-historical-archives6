706f57aedd0a346b00c36d86b834c6597d91ab96
Your Name <you@example.com>
2024-04-28 23:09:48 +0800

    fix

--- 0a34bf00-d592-42a8-9741-835d97fc2aa3.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  "resource_type": "music",
-  "metadata": {"name":"歌唱伟大、光荣、正确的中国共产党","composer":"","description":"","lyrics":[{"lyricist":"","content":"\n\n亿万人民热烈地欢呼放声歌唱\n歌唱毛主席亲手培育的中国共产党\n半个世纪前赴后继英勇奋斗\n党在革命的大风浪中壮大成长\n伟大的党  光荣的党 正确的党\n你是领导我们事业的核心力量\n伟大的导师 敬爱的领袖毛主席\n您是革命人民心中的红太阳\n\n马列主义毛泽东思想光芒万丈\n毛主席的革命路线指引航向\n武装斗争夺取政权 巩固政权\n天安门上五星红旗迎风飘扬\n伟大的党  光荣的党 正确的党\n你是领导我们事业的核心力量\n伟大的导师 敬爱的领袖毛主席\n您是革命人民心中的红太阳\n\n无产阶级文化大革命胜利辉煌\n党在斗争中朝气蓬勃更加坚强\n团结起来高举红旗继续革命\n一定要实现共产主义伟大理想\n伟大的党  光荣的党正确的党\n你是领导我们事业的核心力量\n伟大的导师 敬爱的领袖毛主席\n您是革命人民心中的红太阳\n\n","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/gcwdgrzqdzggcd.mp3","artist":"合唱集体词 王莘曲","source":"http://ip.lidicity.com/hj/cn/hsjd.html#top"}]}],"id":"0a34bf00-d592-42a8-9741-835d97fc2aa3"},
-  "version": 2
-}
--- 0a66de79-e89c-45df-9329-1e35f7f66407.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  "resource_type": "music",
-  "metadata": {"name":"人民公社新花开","composer":"","description":"","lyrics":[{"lyricist":"","content":"\n\n一朵红花向阳开耶\n政治夜校办起来\n学马列那个学文化\n革命生产大步迈\n革命生产大步迈\n\n二朵红花向阳开\n理论队伍建起来\n批林批孔斗志高\n继续革命朝前迈\n继续革命朝前迈\n\n三朵红花向阳开耶\n贫下中农登讲台\n上层建筑飘红旗\n永保江山红万代\n永保江山红万代\n\n四朵红花向阳开\n革命样板戏唱起来\n社员发奋学英雄\n豪情壮志满胸怀\n豪情壮志满胸怀\n\n五朵红花向阳开耶\n文艺宣传队建起来\n演新戏那个唱新歌\n男女老少齐登台\n男女老少齐登台\n\n六朵红花向阳开\n社员挥笔把诗赛\n歌唱文化大革命\n歌唱农业学大寨\n歌唱农业学大寨\n\n七朵红花向阳开\n新图书室办起来\n新书新画新思想\n新人新事新气派\n\n八朵红花向阳开呀\n革命故事讲起来\n文化阵地要占领啊\n齐培养革命新一代\n文化阵地要占领\n培养革命新一代\n\n九朵红花向阳开\n体育活动搞起来\n身强力壮干革命\n龙腾虎跃多豪迈\n\n十朵红花向阳开呀\n革命新风树起来\n晚婚节育退彩礼呀\n齐移风易俗面貌改\n\n人民公社新花开\n人民公社新花开\n毛泽东思想放光彩\n\n","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/renmingongshexinhuakai.mp3","artist":"康冰、孟广来领唱柳向忱、韩伟词 宋国生曲","source":"http://ip.lidicity.com/hj/cn/hsjd.html#top"}]}],"id":"0a66de79-e89c-45df-9329-1e35f7f66407"},
-  "version": 2
-}
