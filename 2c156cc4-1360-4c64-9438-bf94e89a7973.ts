d0fcb5b2ab92075521b51ee1f29a04ca18580611
Your Name <you@example.com>
2024-10-28 00:01:39 +0800

    1

---
+++ 2c156cc4-1360-4c64-9438-bf94e89a7973.ts
@@ -0,0 +1,11 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"毛主席的光辉把炉台照亮","composer":"","description":"","lyrics":[
+    {
+      "lyricist":"","content":
+      "我战斗在金色的炉台上\n这里是毛主席\n到过的地方\n亲切的教导\n时刻在耳边回响\n沸腾的钢水\n映红了我的胸膛\n千座矿山化铁水\n万吨钢材运四方\n汗水伴着钢花飞舞\n红心随着炉火歌唱\n鞍钢宪法放光芒\n万里征途不迷航\n我战斗在金色的炉台上\n这里是毛主席\n到过的地方\n亲切的教导\n时刻在耳边回响\n革命的豪情激荡在我的胸膛\n千座矿山化铁水\n万吨钢材运四方\n汗水伴着钢花飞舞\n红心随着炉火歌唱\n鞍钢宪法放光芒\n万里征途不迷航\n我战斗在金色的炉台上\n这里是毛主席\n到过的地方\n毛主席呀毛主席\n您的光辉把炉台\n永远照亮\n毛主席呀毛主席\n我永远跟您前进\n跟您前进\n百炼成钢","version":"原版","audios":[
+        {"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/027f毛主席的光辉把炉台照亮 阎维文.mp3","artist":"阎维文","source":""}
+      ]}],"id":"2c156cc4-1360-4c64-9438-bf94e89a7973"
+    },
+  "version": 2
+}
