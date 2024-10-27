286bc42fbd57decfc4543d66f1488085b9d9d875
Your Name <you@example.com>
2024-10-27 21:40:55 +0800

    fix

--- 7cc3a38a-4525-4192-b147-f46df02a0058.ts
+++ 7cc3a38a-4525-4192-b147-f46df02a0058.ts
@@ -1,5 +1,12 @@
 export default {
   "resource_type": "music",
-  "entity": {"name":"造反有理","composer":"李劫夫","description":"","lyrics":[{"lyricist":"李劫夫","content":"马克思主义的道理千条万绪归根到底就是一句话\n造反有理\n造反有理\n根据这个道理\n于是就反抗，就斗争，就干社会主义\n马克思主义的道理千条万绪归根到底就是一句话\n造反有理\n造反有理\n根据这个道理\n于是就反抗，就斗争，就干社会主义","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zfyl.mp3","artist":"中央广播文工团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"},{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zfyl_jp.mp3","artist":"日本齿轮座剧团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"}]}],"id":"7cc3a38a-4525-4192-b147-f46df02a0058"},
+  "entity": {
+    "name":"造反有理","composer":"李劫夫","description":"","lyrics":[
+      {"lyricist":"李劫夫","content":"马克思主义的道理千条万绪归根到底就是一句话\n造反有理\n造反有理\n根据这个道理\n于是就反抗，就斗争，就干社会主义\n马克思主义的道理千条万绪归根到底就是一句话\n造反有理\n造反有理\n根据这个道理\n于是就反抗，就斗争，就干社会主义","version":"原版","audios":[
+        {"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zfyl.mp3","artist":"中央广播文工团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"},
+        {"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zfyl_jp.mp3","artist":"日本齿轮座剧团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"},
+        {"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zaofanyiuli.mp3","artist":"齐唱 毛泽东词 劫夫曲","source":"http://ip.lidicity.com/hj/cn/hsjd.html#top"}
+      ]}
+    ],"id":"7cc3a38a-4525-4192-b147-f46df02a0058"},
   "version": 2
 }
--- a67882c3-4d99-4193-8d14-2c06f8507082.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  "resource_type": "music",
-  "entity": {"name":"“造反有理”","composer":"","description":"","lyrics":[{"lyricist":"","content":"马克思主义的道理\n千条万绪 归根结底\n就是一句话 造反有理\n造反有理 根据这个道理\n于是就反抗就斗争\n就干社会主义\n马克思主义的道理\n千条万绪 归根结底\n就是一句话 造反有理\n造反有理 根据这个道理\n于是就反抗就斗争\n就干社会主义\n","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zaofanyiuli.mp3","artist":"齐唱 毛泽东词 劫夫曲","source":"http://ip.lidicity.com/hj/cn/hsjd.html#top"}]}],"id":"a67882c3-4d99-4193-8d14-2c06f8507082"},
-  "version": 2
-}
