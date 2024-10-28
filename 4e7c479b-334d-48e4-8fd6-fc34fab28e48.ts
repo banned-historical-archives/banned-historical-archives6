9082d117a3d486bdb81833fc41490317b06cbdbb
Your Name <you@example.com>
2024-10-28 23:29:04 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e48.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"乘胜前进深入批邓","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "天安门广场掀巨澜，\n英雄的人民挥铁拳。\n反革命逆流被粉碎，\n邓小平复辟迷梦破了产。\n保卫毛主席！保卫党中央！\n保卫毛主席的革命路线！\n保卫毛主席！保卫党中央！\n保卫毛主席的革命路线！\n天安门广场凯歌传，\n党中央两个决议鼓舞咱。\n乘胜前进，深入批邓，\n反修防修永不休战。\n保卫毛主席！保卫党中央！\n保卫毛主席的革命路线！\n保卫毛主席！保卫党中央！\n保卫毛主席的革命路线！",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/乘胜前进深入批邓.mp3",
+      "artist":"","source":""}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e48"},
+    "version": 2
+}
