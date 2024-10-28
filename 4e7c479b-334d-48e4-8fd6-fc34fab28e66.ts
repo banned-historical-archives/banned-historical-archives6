2198cb99f4318aaf0198d83dd4806accdd73a594
Your Name <you@example.com>
2024-10-29 01:27:12 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e66.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"保卫文化大革命","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "新生事物鲜花开，千朵万朵党栽培。邓小平要复辟，阴谋诡计搞破坏，我们坚决不答应，铁拳高高举起来。保卫文化大革命，狠狠批判走资派。保卫文化大革命，狠狠批判走资派。",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/保卫文化大革命.mp3",
+      "artist":"","source":""}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e66"},
+    "version": 2
+}
