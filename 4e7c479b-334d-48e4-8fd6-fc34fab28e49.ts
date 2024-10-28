e7b3095e87faea49aaf3d683719f875c538d27d9
Your Name <you@example.com>
2024-10-28 23:34:09 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e49.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"坚持党的基本路线","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "坚持党的基本路线，阶级斗争是纲。\n沿着这条路线，抓住这个纲，前进有方向。\n它是反修防修的武器，它是夺取胜利的保障。\n牢记党的基本路线，我们要年年讲，月月讲，天天讲。\n坚持党的基本路线，阶级斗争是纲。\n邓小平篡改它，我们决不让，我们决不让。\n坚决反击右倾翻案妖风，粉碎资本主义复辟梦想。\n牢记党的基本路线，我们要年年讲，月月讲，天天讲。",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/坚持党的基本路线.mp3",
+      "artist":"","source":""}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e49"},
+    "version": 2
+}
