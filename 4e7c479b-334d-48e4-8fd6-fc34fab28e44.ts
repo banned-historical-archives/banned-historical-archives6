c1cabb445988dcc35bd254d12b13daa3b466386d
Your Name <you@example.com>
2024-10-28 21:16:14 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e44.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"煤矿工人永远听毛主席的话","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n抓革命促生产，\n奋战煤海志气大。\n艰苦奋斗创大业，\n千难万险踩脚下，\n哎嘿！\n工业发展创新路，\n煤海盛开大庆花，\n煤海盛开大庆花。\n煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n阶级斗争永不忘，\n继续革命催战马。\n批林批孔当闯将，\n狂风恶浪战胜它，\n哎嘿！\n跟着领袖毛主席，\n革命路上大步跨，\n革命路上大步跨。",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/煤矿工人永远听毛主席的话 ⧸ Coal Miners Will Always Follow Chairman Mao's Teachings (1974) [QaVQpfnaKXY].mp3",
+      "artist":"","source":"中国唱片M-2041"}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e44"},
+    "version": 2
+}
