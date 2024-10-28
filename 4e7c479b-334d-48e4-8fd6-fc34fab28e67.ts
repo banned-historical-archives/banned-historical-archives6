a692f2762d82832fb330c1582b7987c84ab34b72
Your Name <you@example.com>
2024-10-29 01:29:23 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e67.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"翻案不得人心","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "历史在前进，在前进，人民要革命，要革命。邓小平搞复辟，亿万人民坚决不答应。翻案不得人心，翻案不得人心。工农兵打冲锋，打冲锋，红小兵紧紧跟，紧紧跟。痛击右倾翻案风，勇敢捍卫文化大革命。翻案不得人心，翻案不得人心。",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/翻案不得人心.mp3",
+      "artist":"","source":""}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e67"},
+    "version": 2
+}
