34e8d2795bd38010985e2d1dab585e7b03b9472e
Your Name <you@example.com>
2024-10-29 00:28:07 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e54.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"资产阶级从哪里进攻，我们就在哪里战斗","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "风雷滚滚，红旗飘扬，革命人民豪情激荡。\n回击右倾翻案风，慷慨高歌上战场。\n大批判，大辩论，革命烈火越烧越旺，剥开邓小平的画皮，铲除修正主义的土壤。\n资产阶级从哪里进攻，我们就在哪里战斗！\n资产阶级从哪里进攻，我们就在哪里战斗！\n跟着毛主席，披荆斩棘向前方，披荆斩棘向前方。\n风雷滚滚，红旗飘扬，革命人民豪情激荡。\n回击右倾翻案风，慷慨高歌上战场。\n反复辟，反倒退，革命洪流势不可挡，保卫文化大革命成果，阶级斗争永不忘。\n资产阶级从哪里进攻，我们就在哪里战斗！\n资产阶级从哪里进攻，我们就在哪里战斗！\n跟着毛主席，披荆斩棘向前方，披荆斩棘向前方。",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/资产阶级从哪里进攻，我们就在哪里战斗.mp3",
+      "artist":"","source":""}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e54"},
+    "version": 2
+}
