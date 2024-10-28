efab829a07287613abaab503550ce8dc97cc7af4
Your Name <you@example.com>
2024-10-28 20:57:19 +0800

    1

---
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e42.ts
@@ -0,0 +1,10 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":"我们是革命的新一代","composer":"","description":"","lyrics":[{
+    "lyricist":"","content":
+    "红旗把路引，豪情满胸怀，\n阳光灿烂照征途，\n我们是革命的新一代。\n大路我们修，荒山我们改，\n机器我们造，矿藏我们开。\n继承先辈革命志，\n一生交给党安排。\n青春似火红，战歌传天外，\n阳光灿烂照征途，\n我们是革命的新一代。\n放眼五大洲，红心连四海，\n联合无产者，创造新世界。\n反帝反修志不移，\n革命路上朝前迈！",
+"version":"原版","audios":[{"url":
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是革命的新一代 ⧸ We Are A New Generation Of Revolutionary Youth (1974) [K1RJWEXw2AA].mp3",
+      "artist":"江西省歌舞团","source":"中国唱片M-2029"}]}],"id":"4e7c479b-334d-48e4-8fd6-fc34fab28e42"},
+    "version": 2
+}
