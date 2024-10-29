cd87acaefab4e88c7345455f594c1fb37070ea55
Your Name <you@example.com>
2024-10-29 22:20:21 +0800

    1

--- 81528bdb-352b-4aa4-8c25-e12573cd8846.ts
+++ 81528bdb-352b-4aa4-8c25-e12573cd8846.ts
@@ -1,5 +1,5 @@
 export default {
   "resource_type": "music",
-  "entity": {"name":"争取胜利","composer":"","description":"","lyrics":[{"lyricist":"","content":"下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zqsl.mp3","artist":"中央广播文工团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"}]}],"id":"81528bdb-352b-4aa4-8c25-e12573cd8846"},
+  "entity": {"name":"下定决心，不怕牺牲，排除万难，去争取胜利","composer":"","description":"","lyrics":[{"lyricist":"","content":"下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！\n下定决心，不怕牺牲，排除万难，去争取胜利！","version":"原版","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zqsl.mp3","artist":"中央广播文工团","source":"http://www.mzdbl.com.cn/yinyue/yulu/index.html"}]}],"id":"81528bdb-352b-4aa4-8c25-e12573cd8846"},
   "version": 2
 }
