8b07d1fc6d94874d2df5288377d38ad4135b35f1
Your Name <you@example.com>
2024-11-01 23:42:47 +0800

    fix

--- a95fe1bb-2f00.ts
+++ a95fe1bb-2f00.ts
@@ -7,7 +7,7 @@ export default {
     "lyrics":[
       {"lyricist":"",
         "content":
-        "",
+        "我们队伍向太阳，\n古田会议决议指引着方向。\n政治统帅军事，党永远指挥枪。\n毛泽东思想武装我们，\n百战百胜不可阻挡不可阻挡！\n我们队伍向太阳，\n古田会议决议指引着方向。\n依靠人民群众，筑起铁壁铜墙。\n为巩固无产阶级专政，\n前进在毛主席的革命路线上！",
         "version":"原版","audios":[
           {"url":
             "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/072古田会议决议指引着方向 沈阳部队政治部宣传队.mp3",
