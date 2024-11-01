e014b6cf8d338aff0a575bcd170166b0c0950f52
Your Name <you@example.com>
2024-11-01 23:49:09 +0800

    1

---
+++ a95fe1bb-2f02.ts
@@ -0,0 +1,23 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":
+    "我们永远是个战斗队",
+    "composer":"",
+    "description":"",
+    "lyrics":[
+      {"lyricist":"",
+        "content":
+        "",
+        "version":"原版","audios":[
+          {"url":
+            "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/165a我们永远是个战斗队 解放军歌舞团.mp3",
+            "artist":"解放军歌舞团",
+            "source":""},
+          {"url":
+            "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/165我们永远是个战斗队 浙江省歌舞团.mp3",
+            "artist":"浙江省歌舞团",
+            "source":""},
+          ]}
+          ],"id":"a95fe1bb-2f02"},
+  "version": 2
+}
