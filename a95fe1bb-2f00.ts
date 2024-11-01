fe90a0910ba513d07100207e7e7ba1c6b582123d
Your Name <you@example.com>
2024-11-01 23:41:57 +0800

    1

---
+++ a95fe1bb-2f00.ts
@@ -0,0 +1,18 @@
+export default {
+  "resource_type": "music",
+  "entity": {"name":
+    "古田会议决议指引着方向",
+    "composer":"",
+    "description":"",
+    "lyrics":[
+      {"lyricist":"",
+        "content":
+        "",
+        "version":"原版","audios":[
+          {"url":
+            "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/072古田会议决议指引着方向 沈阳部队政治部宣传队.mp3",
+            "artist":"沈阳部队政治部宣传队",
+            "source":"中国唱片M-957"}]}
+          ],"id":"a95fe1bb-2f00"},
+  "version": 2
+}
