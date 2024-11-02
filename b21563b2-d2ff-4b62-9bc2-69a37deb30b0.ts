dca950197178db157f0286df47c8394543c1b57b
Your Name <you@example.com>
2024-11-02 11:43:09 +0800

    1

---
+++ b21563b2-d2ff-4b62-9bc2-69a37deb30b0.ts
@@ -0,0 +1,1 @@
+export default {"resource_type":"music","version":2,"entity":{"id":"b21563b2-d2ff-4b62-9bc2-69a37deb30b0","name":"祝毛主席万寿无疆","composer":"","description":"","lyrics":[{"content":"","version":"原版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/z2祝毛主席万寿无疆.mp3","artist":"","source":""}]}]}};
