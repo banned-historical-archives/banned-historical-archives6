b60454a0732d4a009fcab0a04416da9d75222a6e
Your Name <you@example.com>
2024-11-03 11:17:24 +0800

    1

---
+++ eb5fccb1-c10f-45583.ts
@@ -0,0 +1,25 @@
+export default {
+  "resource_type": "music",
+  "version": 2,
+  "entity": {
+    "id": "eb5fccb1-c10f-45583",
+    "name": "华主席登上天安门-儿歌",
+    "composer": "",
+    "description": "",
+    "tags": [],
+    "lyrics": [
+      {
+        "content": "北京城，飞彩虹，\n华主席登上天安门，\n驱散乌云除“四害”，\n全国人民齐欢腾。\n\n北京城，飞彩虹，\n华主席登上天安门，\n我们有了好舵手，\n继续革命向前进。",
+        "version": "原版",
+        "lyrictist": "",
+        "audios": [
+          {
+            "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/华主席登上天安门-儿歌.mp3",
+            "artist": "北京市少年宫合唱队，中央人民广播电台少年广播合唱团",
+            "source": "中国唱片M-2293"
+          }
+        ]
+      }
+    ]
+  }
+};
