0d908f2b11b47a895c1469afc320b1a86e0b48a5
Your Name <you@example.com>
2024-11-03 13:01:09 +0800

    1

--- 53288a3b-f001-4c0a-8849-8785f17c992a.ts
+++ 53288a3b-f001-4c0a-8849-8785f17c992a.ts
@@ -1,1 +1,8 @@
-export default {"resource_type":"music","version":2,"entity":{"id":"53288a3b-f001-4c0a-8849-8785f17c992a","name":"我们多么幸福","composer":"","description":"","lyrics":[{"content":"","version":"原版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福.mp3","artist":"","source":""}]}]}};
+export default {"resource_type":"music","version":2,"entity":{"id":"53288a3b-f001-4c0a-8849-8785f17c992a","name":"我们多么幸福","composer":"","description":"","lyrics":[
+    {
+    "content":"我们的生活多么幸福，我们的学习多么快乐\n我们感谢亲爱的毛主席，建设许多美丽的学校\n无论在城市还是乡村，所有的孩子都去学习\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐\n每天的早晨背着书包\n我们高高兴兴走进校门\n今天我们跟着老师\n学习本领锻炼身体\n明天我们就像小鸟一样\n飞向祖国建设的岗位\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐\n\n快乐的今日，穿着新衣\n抱着鲜花排好队伍去游行\n多少英雄向我们招手\n多少朋友向我们欢呼\n都说我们将来建设祖国\n每一个都是英雄模范\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐"
+    ,"version":"原版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福.mp3","artist":"","source":""}]},
+    {
+    "content":"我们的生活多么幸福，我们的学习多么快乐\n晨风吹风五星红旗，彩霞染红万里山河\n无论在城市还是乡村，家家的孩子都去上学\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐\n勇敢的海燕穿过白云\n我们高高兴兴走进校门\n今天我们跟着老师\n学习科学，学习本领\n明天我们就像小鸟一样\n飞向祖国工矿农村\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐"
+    ,"version":"改开版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福-银河少年合唱团.mp3","artist":"","source":""}]}
+]}};
