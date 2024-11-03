e299d7bf19fe1f2fdfe90cb01a2cac236a23cf12
Your Name <you@example.com>
2024-11-03 13:01:49 +0800

    1

--- 53288a3b-f001-4c0a-8849-8785f17c992a.ts
+++ 53288a3b-f001-4c0a-8849-8785f17c992a.ts
@@ -4,5 +4,5 @@ export default {"resource_type":"music","version":2,"entity":{"id":"53288a3b-f00
     ,"version":"原版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福.mp3","artist":"","source":""}]},
     {
     "content":"我们的生活多么幸福，我们的学习多么快乐\n晨风吹风五星红旗，彩霞染红万里山河\n无论在城市还是乡村，家家的孩子都去上学\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐\n勇敢的海燕穿过白云\n我们高高兴兴走进校门\n今天我们跟着老师\n学习科学，学习本领\n明天我们就像小鸟一样\n飞向祖国工矿农村\n哈哈哈哈哈，哈哈哈哈哈\n我们的生活多么幸福\n哈哈哈哈哈，哈哈哈哈哈\n我们的学习多么快乐"
-    ,"version":"改开版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福-银河少年合唱团.mp3","artist":"","source":""}]}
+    ,"version":"改开版","lyrictist":"","audios":[{"url":"https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们多么幸福-银河少年合唱团.mp3","artist":"银河少年合唱团","source":""}]}
 ]}};
