f30099391ef8ec61ea9016ff1153404fb84de564
Your Name <you@example.com>
2024-11-30 02:20:17 +0800

    fix

--- dd7c8e7c-c6f9-4f90-b0c7-54017f5790b2.ts
+++ dd7c8e7c-c6f9-4f90-b0c7-54017f5790b2.ts
@@ -26,6 +26,28 @@ export default {
           '胡松华',
         ],
       },
+      {
+        content: '啊 嘿咿耶 啊……\n\n        从草原来到毛主席的身旁\n        手捧宝书把赞歌唱\n        感谢伟大的共产党\n        祝福毛主席万寿无疆\n        \n        英雄的祖国屹立在东方\n        像初升的太阳光芒万丈\n        各民族人民跟着毛主席\n        奔向共产主义前方\n        \n        啊 嗬咿……',
+        version: '改版',
+        audios: [
+          {
+            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zange2.mp3',
+            sources: [],
+            artists: [
+              {
+                name: '内蒙古乌兰牧骑毛泽东思想宣传队',
+                type: '独唱',
+              },
+            ],
+            art_forms: [
+              '独唱',
+            ],
+          },
+        ],
+        lyricists: [
+          '集体改',
+        ],
+      },
     ],
     id: 'dd7c8e7c-c6f9-4f90-b0c7-54017f5790b2',
     composers: [
