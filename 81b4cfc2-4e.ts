e7e92ded465fa4f0d705d4467a09db59f70715e0
Your Name <you@example.com>
2025-01-01 13:46:57 +0800

    1

---
+++ 81b4cfc2-4e.ts
@@ -0,0 +1,29 @@
+export default {
+  resource_type: 'music',
+  entity: {
+    name: '热烈庆祝党的九大胜利召开',
+    description: '',
+    lyrics: [
+      {
+        content: '五湖四海响春雷，七亿神州尽朝晖，热烈庆祝党的九大胜利召开。纵情欢呼毛主席，万岁万岁万万岁，伟大光荣正确的党，毛泽东思想来武装，大风浪里千锤百炼，朝气蓬勃斗志昂，永远跟着毛主席，向前进，阔步奔向共产主义前方！五湖四海响春雷，七亿神州尽朝晖，热烈庆祝党的九大胜利召开。纵情欢呼毛主席，万岁万岁万万岁。伟大光荣正确的党，毛泽东思想来武装，高举马克思列宁主义大旗，坚决把帝修反彻底埋葬，非拉人民起来，团结战斗，阔步奔向共产主义前方！',
+        version: '原版',
+        audios: [
+          {
+            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/热烈庆祝党的九大胜利召开.mp3',
+            sources: [],
+            artists: [
+            ],
+            art_forms: [
+            ],
+          },
+        ],
+        lyricists: [
+        ],
+      },
+    ],
+    id: '81b4cfc2-4e',
+    composers: [
+    ],
+  },
+  version: 2,
+};
