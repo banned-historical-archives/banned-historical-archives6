2f15a54e6c6159157590fc8291e17116f412ddba
Your Name <you@example.com>
2025-12-14 05:27:49 +0800

    1

--- ec565392-1acb-44fa-a5c9-f7da03e6baa3.ts
+++ ec565392-1acb-44fa-a5c9-f7da03e6baa3.ts
@@ -1,27 +1,48 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "ec565392-1acb-44fa-a5c9-f7da03e6baa3",
-        "name": "苗岭连北京",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "ec565392-1acb-44fa-a5c9-f7da03e6baa3",
+        name: "苗岭连北京",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "山泉清又清喔 山泉清又清\n春茶绿茵茵喔 春茶绿茵茵\n筛碗苗山茶 献给解放军\n山区修铁路 军民一条心\n遇水架桥梁 逢山打岩洞\n汗水洒苗岭 军民情谊深\n苗山飞彩虹 火车进山村\n幸福装满车 军民齐欢腾\n感谢毛主席 恩情唱不尽\n一条幸福线 苗岭连北京\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/苗岭连北京.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/苗岭连北京.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/miaolinglianbeijing.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                            {
+                                name: "李怀德",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "彭承标",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "白诚仁编",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
