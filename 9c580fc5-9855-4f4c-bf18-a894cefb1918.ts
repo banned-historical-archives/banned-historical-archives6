e557f6d61cdecb437c221cadee01e6e33b46ad73
Your Name <you@example.com>
2025-12-14 04:41:41 +0800

    wip

--- 9c580fc5-9855-4f4c-bf18-a894cefb1918.ts
+++ 9c580fc5-9855-4f4c-bf18-a894cefb1918.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "9c580fc5-9855-4f4c-bf18-a894cefb1918",
-        "name": "我们是光荣的人民教师",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "9c580fc5-9855-4f4c-bf18-a894cefb1918",
+        name: "我们是光荣的人民教师",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是光荣的人民教师.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是光荣的人民教师.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/540我们是光荣的人民教师 长影乐团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "长影乐团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
