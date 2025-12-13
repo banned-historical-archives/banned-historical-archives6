e4d2ae96ea85503d96d3088c9d0f6d1a56f8d94e
Your Name <you@example.com>
2025-12-14 04:20:10 +0800

    wip

--- c2c7e351-e0a2-49c3-85e0-3b22ebe0ce41.ts
+++ c2c7e351-e0a2-49c3-85e0-3b22ebe0ce41.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'c2c7e351-e0a2-49c3-85e0-3b22ebe0ce41',
-    name: '政策和策略是党的生命',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl109x政策和策略是党的生命.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "c2c7e351-e0a2-49c3-85e0-3b22ebe0ce41",
+        name: "政策和策略是党的生命",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl109x政策和策略是党的生命.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl109xa政策和策略是党的生命.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
