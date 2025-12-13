9517eaae5ab7472365be0146ca3393faeac74087
Your Name <you@example.com>
2025-12-14 05:17:17 +0800

    1

--- 80276d5e-d50a-4fb0-a46b-12bf57e805da.ts
+++ 80276d5e-d50a-4fb0-a46b-12bf57e805da.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "80276d5e-d50a-4fb0-a46b-12bf57e805da",
-        "name": "青春战歌",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "80276d5e-d50a-4fb0-a46b-12bf57e805da",
+        name: "青春战歌",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/青春战歌.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/青春战歌.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/544青春战歌 解放军歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "解放军歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
