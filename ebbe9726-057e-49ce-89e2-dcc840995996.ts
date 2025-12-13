f618ae6a8347514130555a0275884d8fd364e456
Your Name <you@example.com>
2025-12-14 06:05:36 +0800

    1

--- ebbe9726-057e-49ce-89e2-dcc840995996.ts
+++ ebbe9726-057e-49ce-89e2-dcc840995996.ts
@@ -1,28 +1,36 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'ebbe9726-057e-49ce-89e2-dcc840995996',
-    name: '文化革命打先锋',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/428文化革命打先锋.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "ebbe9726-057e-49ce-89e2-dcc840995996",
+        name: "文化革命打先锋",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/428文化革命打先锋.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/文化革命打先锋.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
