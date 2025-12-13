bf8d0545754368fb1d6f0bc793d2cd6683242183
Your Name <you@example.com>
2025-12-14 05:43:18 +0800

    1

--- 4e7c479b-334d-48e4-8fd6-fc34fab28e44.ts
+++ 4e7c479b-334d-48e4-8fd6-fc34fab28e44.ts
@@ -1,34 +1,42 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '煤矿工人永远听毛主席的话',
-    description: '',
-    lyrics: [
-      {
-        content: '煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n抓革命促生产，\n奋战煤海志气大。\n艰苦奋斗创大业，\n千难万险踩脚下，\n哎嘿！\n工业发展创新路，\n煤海盛开大庆花，\n煤海盛开大庆花。\n煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n阶级斗争永不忘，\n继续革命催战马。\n批林批孔当闯将，\n狂风恶浪战胜它，\n哎嘿！\n跟着领袖毛主席，\n革命路上大步跨，\n革命路上大步跨。',
-        version: '原版',
-        audios: [
-          {
-            url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/煤矿工人永远听毛主席的话 ⧸ Coal Miners Will Always Follow Chairman Mao's Teachings (1974) [QaVQpfnaKXY].mp3",
-            sources: [
-              '中国唱片M-2041',
-            ],
-            artists: [
-              {
-                name: '山西省人民歌舞团合唱队，吕梁手风琴',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "煤矿工人永远听毛主席的话",
+        description: "",
+        lyrics: [
+            {
+                content: "煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n抓革命促生产，\n奋战煤海志气大。\n艰苦奋斗创大业，\n千难万险踩脚下，\n哎嘿！\n工业发展创新路，\n煤海盛开大庆花，\n煤海盛开大庆花。\n煤矿工人永远听毛主席的话，\n毛主席说啥咱干咱。\n阶级斗争永不忘，\n继续革命催战马。\n批林批孔当闯将，\n狂风恶浪战胜它，\n哎嘿！\n跟着领袖毛主席，\n革命路上大步跨，\n革命路上大步跨。",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/煤矿工人永远听毛主席的话 ⧸ Coal Miners Will Always Follow Chairman Mao's Teachings (1974) [QaVQpfnaKXY].mp3",
+                        sources: [
+                            "中国唱片M-2041",
+                        ],
+                        artists: [
+                            {
+                                name: "山西省人民歌舞团合唱队，吕梁手风琴",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/煤矿工人永远听毛主席的话.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: '4e7c479b-334d-48e4-8fd6-fc34fab28e44',
-    composers: [],
-  },
-  version: 2,
+        id: "4e7c479b-334d-48e4-8fd6-fc34fab28e44",
+        composers: [],
+    },
+    version: 2,
 };
