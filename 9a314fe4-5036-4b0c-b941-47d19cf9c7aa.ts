e275ddcd86b37e73077b13fbef1795f5f4f3e35b
Your Name <you@example.com>
2025-12-14 04:38:00 +0800

    wip

--- 9a314fe4-5036-4b0c-b941-47d19cf9c7aa.ts
+++ 9a314fe4-5036-4b0c-b941-47d19cf9c7aa.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '9a314fe4-5036-4b0c-b941-47d19cf9c7aa',
-    name: '军民团结如一人，试看天下谁能敌？',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl91军民团结如一人，试看天下谁能敌？.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "9a314fe4-5036-4b0c-b941-47d19cf9c7aa",
+        name: "军民团结如一人，试看天下谁能敌？",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl91军民团结如一人，试看天下谁能敌？.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl91x军民团结如一人，试看天下谁能敌？.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
