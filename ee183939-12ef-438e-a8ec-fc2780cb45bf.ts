06dfe389383fb736a5ff455d165b5073a5fbf67a
Your Name <you@example.com>
2025-12-14 05:48:55 +0800

    1

--- ee183939-12ef-438e-a8ec-fc2780cb45bf.ts
+++ ee183939-12ef-438e-a8ec-fc2780cb45bf.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '毛主席走遍祖国大地',
-    description: '',
-    lyrics: [
-      {
-        content: '毛主席走遍祖国大地\n锦绣河山更加壮丽\n更加壮丽\n任凭征途风云变幻\n鲜红的太阳永放光辉\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来  团结起来\n争取更大的胜利\n毛主席走遍祖国大地\n革命人民永远在一起\n永远在一起\n亲切关怀鼓舞我们\n谆谆的教导铭刻在心里\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来 团结起来\n争取更大的胜利\n毛主席走遍祖国大地\n东风劲吹晴空万里\n晴空万里\n革命航船乘风破浪\n英雄的人民所向无敌\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来 团结起来\n争取更大的胜利\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/maozhuxizoubianzuguodadi.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '龚军工、顾企兰',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "毛主席走遍祖国大地",
+        description: "",
+        lyrics: [
+            {
+                content: "毛主席走遍祖国大地\n锦绣河山更加壮丽\n更加壮丽\n任凭征途风云变幻\n鲜红的太阳永放光辉\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来  团结起来\n争取更大的胜利\n毛主席走遍祖国大地\n革命人民永远在一起\n永远在一起\n亲切关怀鼓舞我们\n谆谆的教导铭刻在心里\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来 团结起来\n争取更大的胜利\n毛主席走遍祖国大地\n东风劲吹晴空万里\n晴空万里\n革命航船乘风破浪\n英雄的人民所向无敌\n毛主席率领我们奋勇前进\n高举起马列主义伟大红旗\n毛主席率领我们奋勇前进\n团结起来 团结起来\n争取更大的胜利\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/maozhuxizoubianzuguodadi.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "龚军工、顾企兰",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席走遍祖国大地.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "辽宁省革命样板戏学习班刘文玉",
+                ],
+            },
         ],
-        lyricists: [
-          '辽宁省革命样板戏学习班刘文玉',
+        id: "ee183939-12ef-438e-a8ec-fc2780cb45bf",
+        composers: [
+            "秦咏诚",
         ],
-      },
-    ],
-    id: 'ee183939-12ef-438e-a8ec-fc2780cb45bf',
-    composers: [
-      '秦咏诚',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
