04520e530f7a8a63caac170c0511a8d2b5819c64
Your Name <you@example.com>
2025-12-14 05:30:24 +0800

    1

--- 5fe39dc9-0de8-488e-a635-3bea89da8689.ts
+++ 5fe39dc9-0de8-488e-a635-3bea89da8689.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '5fe39dc9-0de8-488e-a635-3bea89da8689',
-    name: '胜利永远属于我们',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/581胜利永远属于我们 广州部队政治部文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '广州部队政治部文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "5fe39dc9-0de8-488e-a635-3bea89da8689",
+        name: "胜利永远属于我们",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/581胜利永远属于我们 广州部队政治部文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "广州部队政治部文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/胜利永远属于我们.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
