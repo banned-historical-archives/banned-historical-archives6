553ff8c99f83c8ae80078c6c98659789465b8ac3
Your Name <you@example.com>
2025-12-14 03:33:43 +0800

    1

--- 126a90a1-6e7b-4468-8e3b-d9636e1b6665.ts
+++ 126a90a1-6e7b-4468-8e3b-d9636e1b6665.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '红区干部好作风',
-    description: '',
-    lyrics: [
-      {
-        content: '哎呀咧\n红区（格）干部是好作风\n（哎里格）自带干粮\n去办（呀哈格）公\n（哎呀）日穿草鞋分田地\n（哎呀）同志（格）\n夜打灯笼\n访贫（呀哈格）农\n哎呀咧\n红区（格）干部是好作风\n（哎里格）真心实意\n为群（呀哈格）众\n（哎呀）柴米油盐都想到\n（哎呀）同志（格）\n问寒问暖\n情意（呀哈格）重\n哎呀咧\n红区（格）干部是好作风\n（哎里格）毛委员亲手\n培养（呀哈格）成\n（哎呀）领导群众闹革命\n（哎呀）同志（格）\n艰苦奋斗\n人称（呀哈格）颂\n哎呀咧  哎呀咧\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/hongquganbuhaozuofong.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '马孟丹、罗德成',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-              '江西客家民歌',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "红区干部好作风",
+        description: "",
+        lyrics: [
+            {
+                content: "哎呀咧\n红区（格）干部是好作风\n（哎里格）自带干粮\n去办（呀哈格）公\n（哎呀）日穿草鞋分田地\n（哎呀）同志（格）\n夜打灯笼\n访贫（呀哈格）农\n哎呀咧\n红区（格）干部是好作风\n（哎里格）真心实意\n为群（呀哈格）众\n（哎呀）柴米油盐都想到\n（哎呀）同志（格）\n问寒问暖\n情意（呀哈格）重\n哎呀咧\n红区（格）干部是好作风\n（哎里格）毛委员亲手\n培养（呀哈格）成\n（哎呀）领导群众闹革命\n（哎呀）同志（格）\n艰苦奋斗\n人称（呀哈格）颂\n哎呀咧  哎呀咧\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/hongquganbuhaozuofong.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "马孟丹、罗德成",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                            "江西客家民歌",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/红区干部好作风.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: '126a90a1-6e7b-4468-8e3b-d9636e1b6665',
-    composers: [],
-  },
-  version: 2,
+        id: "126a90a1-6e7b-4468-8e3b-d9636e1b6665",
+        composers: [],
+    },
+    version: 2,
 };
--- zgcpm852-5.ts
+++ zgcpm852-5.ts
@@ -1,29 +1,40 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '跟着毛主席在大风大浪中前进',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/跟着毛主席在大风大浪中前进.mp3',
-            sources: ['中国唱片m-852'],
-            artists: [
-            ],
-            art_forms: [
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "跟着毛主席在大风大浪中前进",
+        description: "",
+        lyrics: [
+            {
+                content: "战鼓敲 军号响\n毛泽东思想伟大红旗\n高高飘扬\n革命大军奋勇前进\n排山倒海无阻挡\n天不怕 地不怕\n要把旧世界彻底埋葬\n我们革命造反者\n毛泽东思想来武装\n敢斗争 敢胜利\n要把新世界开创\n誓死保卫毛主席\n誓死保卫党中央\n保卫毛主席 保卫党中央\n保卫毛主席 保卫党中央\n跟着毛主席\n在大风大浪中前进\n永远向前方\n永远向前方\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/跟着毛主席在大风大浪中前进.mp3",
+                        sources: [
+                            "中国唱片m-852",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/genzhemaozhuxizaidafengdalangzhongqianjin.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [
-        ],
-      },
-    ],
-    id: 'zgcpm852-5',
-    composers: [
-    ],
-  },
-  version: 2,
+        id: "zgcpm852-5",
+        composers: [],
+    },
+    version: 2,
 };
