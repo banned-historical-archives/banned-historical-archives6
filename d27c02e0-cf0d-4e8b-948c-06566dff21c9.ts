3a78b9db8e74e1f077b391f72471e756f5d452c7
Your Name <you@example.com>
2025-12-14 05:18:10 +0800

    1

--- d27c02e0-cf0d-4e8b-948c-06566dff21c9.ts
+++ d27c02e0-cf0d-4e8b-948c-06566dff21c9.ts
@@ -1,40 +1,48 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '阿佤人民唱新歌',
-    description: '',
-    lyrics: [
-      {
-        content: '村村寨寨哎打起鼓敲起锣\n阿佤唱新歌\n毛主席光辉照边疆\n山笑水笑人欢乐\n人民公社好 架起幸福桥\n哎 道路越走越宽阔\n越宽阔哎 江三木罗\n山山岭岭哎歌声起红旗飘\n闪闪银锄落\n毛主席号召学大寨\n清清河水上山坡\n茶园绿油油哎梯田翻金波\n哎 大寨花开千万朵\n千万朵哎 江三木罗\n各族人民哎团结紧向前进\n壮志震山河\n毛主席怎样说\n阿佤人民就怎样做\n跟著毛主席哎跟著共产党\n哎 阿佤人民唱新歌\n唱新歌哎 江三木罗\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/awarenminchangxinge.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '合唱',
-                type: '独唱',
-              },
-              {
-                name: '集体改词',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "阿佤人民唱新歌",
+        description: "",
+        lyrics: [
+            {
+                content: "村村寨寨哎打起鼓敲起锣\n阿佤唱新歌\n毛主席光辉照边疆\n山笑水笑人欢乐\n人民公社好 架起幸福桥\n哎 道路越走越宽阔\n越宽阔哎 江三木罗\n山山岭岭哎歌声起红旗飘\n闪闪银锄落\n毛主席号召学大寨\n清清河水上山坡\n茶园绿油油哎梯田翻金波\n哎 大寨花开千万朵\n千万朵哎 江三木罗\n各族人民哎团结紧向前进\n壮志震山河\n毛主席怎样说\n阿佤人民就怎样做\n跟著毛主席哎跟著共产党\n哎 阿佤人民唱新歌\n唱新歌哎 江三木罗\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/awarenminchangxinge.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                            {
+                                name: "集体改词",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/阿佤人民唱新歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "杨正仁",
+                ],
+            },
         ],
-        lyricists: [
-          '杨正仁',
+        id: "d27c02e0-cf0d-4e8b-948c-06566dff21c9",
+        composers: [
+            "杨正仁",
         ],
-      },
-    ],
-    id: 'd27c02e0-cf0d-4e8b-948c-06566dff21c9',
-    composers: [
-      '杨正仁',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
