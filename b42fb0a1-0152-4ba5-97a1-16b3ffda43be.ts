5d752ffa46b0dfb278ec13c7be448a21529f490e
Your Name <you@example.com>
2025-12-14 04:20:46 +0800

    wip

--- b42fb0a1-0152-4ba5-97a1-16b3ffda43be.ts
+++ b42fb0a1-0152-4ba5-97a1-16b3ffda43be.ts
@@ -1,27 +1,44 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "b42fb0a1-0152-4ba5-97a1-16b3ffda43be",
-        "name": "天山青松根连根",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "b42fb0a1-0152-4ba5-97a1-16b3ffda43be",
+        name: "天山青松根连根",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "天山上的青松根连着根\n各族人民亲又亲\n各族人民亲又亲\n团结胜利向前进 向前进\n建设边疆好山河\n保卫祖国万年春\n建设边疆好山河\n保卫祖国万年春\n天山上的青松数不清\n千歌万曲唱不尽\n千歌万曲唱不尽\n高唱赞歌向北京 向北京\n隔山隔水不隔音\n毛主席和我们心连心\n隔山隔水不隔音\n毛主席和我们心连心\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/天山青松根连根.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/天山青松根连根.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/tianshanqingsonggenliangen.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "热比亚买买提",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "蒋德夫",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "周吉",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
