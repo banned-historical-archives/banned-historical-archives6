f3a5520e6a12efd27dd4169d50def277008e3268
Your Name <you@example.com>
2025-12-14 04:34:03 +0800

    wip

--- a341d0b3-6beb-4fa0-a579-a2dff0e93f06.ts
+++ a341d0b3-6beb-4fa0-a579-a2dff0e93f06.ts
@@ -1,33 +1,46 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'a341d0b3-6beb-4fa0-a579-a2dff0e93f06',
-    name: '四个念念不忘',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/430四个念念不忘 中央广播文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中央广播文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "a341d0b3-6beb-4fa0-a579-a2dff0e93f06",
+        name: "四个念念不忘",
+        description: "",
+        lyrics: [
+            {
+                content: "万杆红旗迎东风\n人民紧紧跟着毛泽东\n世世代代干革命\n党的教导记心中 记心中\n念念不忘阶级斗争\n念念不忘无产阶级专政\n紧握枪杆掌大印\n红色江山代代红\n红色江山代代红\n万杆红旗迎东风\n人民紧紧跟着毛泽东\n世世代代干革命\n党的教导记心中 记心中\n念念不忘突出政治\n念念不忘高举\n毛泽东思想伟大红旗\n反帝反修斗到底\n世界革命定胜利\n世界革命一定胜利 一定胜利\n",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/430四个念念不忘 中央广播文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央广播文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/4gnnbw.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "齐唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
