e48ce98e60d7f75490961ef55c42e1fbb05a4eeb
Your Name <you@example.com>
2025-12-14 05:42:04 +0800

    1

--- 77d7d254-7f61-438b-8b85-4603c92f52ba.ts
+++ 77d7d254-7f61-438b-8b85-4603c92f52ba.ts
@@ -1,36 +1,52 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '牧区大寨红旗飘',
-    description: '',
-    lyrics: [
-      {
-        content: '\n在那毛乌素沙漠中\n有一条绿色长城\n那是乌审召人的汗水\n浇灌的茂密树林\n在那鄂尔多斯草原上\n有一面明镜\n那是乌审召人引来泉水\n引来泉水清清\n啊哈嗬伊\n啊哈哈啊哈哈嗬伊\n乌审召呀乌审召\n牧区大寨红旗飘\n乌审召呀乌审召\n永远前进在革命的大道\n在那毛乌素沙漠中\n飘荡着战斗歌声\n那是乌审召的人们\n向沙漠英勇进军\n在那鄂尔多斯草原上\n红日东升\n那是毛泽东思想\n照亮各族人民\n照亮各族人民前程\n啊哈嗬伊\n啊哈哈啊哈哈嗬伊\n乌审召呀乌审召\n牧区大寨红旗飘\n乌审召呀乌审召\n永远前进在革命的大道\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/muqudazhaihongqipiao.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中国歌剧团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "牧区大寨红旗飘",
+        description: "",
+        lyrics: [
+            {
+                content: "\n在那毛乌素沙漠中\n有一条绿色长城\n那是乌审召人的汗水\n浇灌的茂密树林\n在那鄂尔多斯草原上\n有一面明镜\n那是乌审召人引来泉水\n引来泉水清清\n啊哈嗬伊\n啊哈哈啊哈哈嗬伊\n乌审召呀乌审召\n牧区大寨红旗飘\n乌审召呀乌审召\n永远前进在革命的大道\n在那毛乌素沙漠中\n飘荡着战斗歌声\n那是乌审召的人们\n向沙漠英勇进军\n在那鄂尔多斯草原上\n红日东升\n那是毛泽东思想\n照亮各族人民\n照亮各族人民前程\n啊哈嗬伊\n啊哈哈啊哈哈嗬伊\n乌审召呀乌审召\n牧区大寨红旗飘\n乌审召呀乌审召\n永远前进在革命的大道\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/muqudazhaihongqipiao.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中国歌剧团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/牧区大寨红旗飘.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/牧区大寨红旗飘1.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "张之涛",
+                ],
+            },
         ],
-        lyricists: [
-          '张之涛',
+        id: "77d7d254-7f61-438b-8b85-4603c92f52ba",
+        composers: [
+            "王世一",
         ],
-      },
-    ],
-    id: '77d7d254-7f61-438b-8b85-4603c92f52ba',
-    composers: [
-      '王世一',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
