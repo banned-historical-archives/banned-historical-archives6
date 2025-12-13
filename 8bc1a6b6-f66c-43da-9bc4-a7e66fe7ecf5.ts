aa115872459ceb1cc0d643d2c0b23b4e0cafe197
Your Name <you@example.com>
2025-12-14 06:09:29 +0800

    1

--- 8bc1a6b6-f66c-43da-9bc4-a7e66fe7ecf5.ts
+++ 8bc1a6b6-f66c-43da-9bc4-a7e66fe7ecf5.ts
@@ -1,27 +1,48 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "8bc1a6b6-f66c-43da-9bc4-a7e66fe7ecf5",
-        "name": "我爱这蓝色的海洋",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "8bc1a6b6-f66c-43da-9bc4-a7e66fe7ecf5",
+        name: "我爱这蓝色的海洋",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "我爱这蓝色的海洋\n祖国的海疆壮丽宽广\n我爱海岸耸立的山峰\n俯瞰着海面像哨兵一样\n啊 海军战士红心向党\n严阵以待紧握钢枪\n我守卫在海防线上\n保卫着祖国无尚荣光\n我爱这蓝色的海洋\n祖国的海疆有丰富的宝藏\n我爱晴朗辽阔的海空\n英雄的战鹰在展翅飞翔\n啊 穿云雾跨海浪\n海空战士胸有朝阳\n我守卫在海防线上\n保卫着祖国无尚荣光\n我爱这蓝色的海洋\n矫健的海燕\n在暴风雨里成长\n我爱大海的惊涛骇浪\n把我们锻炼得无比坚强\n啊 战舰奔驰劈涛斩浪\n毛主席挥手指引航向\n我守卫在海防线上\n保卫着祖国无尚荣光\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我爱这蓝色的海洋.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我爱这蓝色的海洋.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/woaizhelansedehaiyang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "胡宝善",
+                                type: "独唱",
+                            },
+                            {
+                                name: "胡宝善",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "王传流",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "胡宝善",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
