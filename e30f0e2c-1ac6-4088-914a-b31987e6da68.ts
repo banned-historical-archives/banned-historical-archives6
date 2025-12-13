52266f70ed9ae890870b93f6d932662c66a593a9
Your Name <you@example.com>
2025-12-14 05:59:14 +0800

    1

--- e30f0e2c-1ac6-4088-914a-b31987e6da68.ts
+++ e30f0e2c-1ac6-4088-914a-b31987e6da68.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '歌唱南京路上好八连',
-    description: '',
-    lyrics: [
-      {
-        content: '好八连 好在啥地方\n好在忠于人民忠于党\n站在南京路 心向红太阳\n路线分得清 敢闯大风浪\n为人民 几十年 美名传四方\n歌唱好八连 学习好八连\n毛主席的教导要时刻记在心上\n好八连 好在啥地方\n好在两个务必永不忘\n工农是老师 社会做课堂\n草鞋针线包 代代闪金光\n拒腐蚀 永不沾 红旗迎风扬\n歌唱好八连 学习好八连\n继续革命奔向那胜利的前方\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/gcnjlsh8l.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '合唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "歌唱南京路上好八连",
+        description: "",
+        lyrics: [
+            {
+                content: "好八连 好在啥地方\n好在忠于人民忠于党\n站在南京路 心向红太阳\n路线分得清 敢闯大风浪\n为人民 几十年 美名传四方\n歌唱好八连 学习好八连\n毛主席的教导要时刻记在心上\n好八连 好在啥地方\n好在两个务必永不忘\n工农是老师 社会做课堂\n草鞋针线包 代代闪金光\n拒腐蚀 永不沾 红旗迎风扬\n歌唱好八连 学习好八连\n继续革命奔向那胜利的前方\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/gcnjlsh8l.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/歌唱南京路上好八连.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "黎汝清",
+                ],
+            },
         ],
-        lyricists: [
-          '黎汝清',
+        id: "e30f0e2c-1ac6-4088-914a-b31987e6da68",
+        composers: [
+            "沈亚威",
         ],
-      },
-    ],
-    id: 'e30f0e2c-1ac6-4088-914a-b31987e6da68',
-    composers: [
-      '沈亚威',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
