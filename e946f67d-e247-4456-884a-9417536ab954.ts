ea73badfd1c3d25538171c496985b3bf919643eb
Your Name <you@example.com>
2025-12-14 05:06:40 +0800

    1

--- e946f67d-e247-4456-884a-9417536ab954.ts
+++ e946f67d-e247-4456-884a-9417536ab954.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'e946f67d-e247-4456-884a-9417536ab954',
-    name: '首都民兵之歌',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/336首都民兵之歌 中央广播文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中央广播文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "e946f67d-e247-4456-884a-9417536ab954",
+        name: "首都民兵之歌",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/336首都民兵之歌 中央广播文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央广播文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/首都民兵之歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
