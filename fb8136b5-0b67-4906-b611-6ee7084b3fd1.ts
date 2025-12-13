367609a6279425da8e02b7178cdce9c5089d602c
Your Name <you@example.com>
2025-12-14 06:07:31 +0800

    1

--- fb8136b5-0b67-4906-b611-6ee7084b3fd1.ts
+++ fb8136b5-0b67-4906-b611-6ee7084b3fd1.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '抗日战歌',
-    description: '',
-    lyrics: [
-      {
-        content: '\n跟着毛主席 万众一条心\n全国同胞们 奋勇向前进\n我们是抗日的队伍\n我们要团结紧\n高举革命红旗\n勇敢杀敌人\n跟着毛主席 万众一条心\n高举革命红旗胜利向前进\n跟着毛主席 万众一条心\n紧握手中枪 歼灭侵略军\n我们是抗日的队伍\n我们要团结紧\n高举革命红旗\n勇敢杀敌人\n跟着毛主席 万众一条心\n高举革命红旗胜利向前进\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/kangrizhange.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中央乐团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "抗日战歌",
+        description: "",
+        lyrics: [
+            {
+                content: "\n跟着毛主席 万众一条心\n全国同胞们 奋勇向前进\n我们是抗日的队伍\n我们要团结紧\n高举革命红旗\n勇敢杀敌人\n跟着毛主席 万众一条心\n高举革命红旗胜利向前进\n跟着毛主席 万众一条心\n紧握手中枪 歼灭侵略军\n我们是抗日的队伍\n我们要团结紧\n高举革命红旗\n勇敢杀敌人\n跟着毛主席 万众一条心\n高举革命红旗胜利向前进\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/kangrizhange.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央乐团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/抗日战歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "集体",
+                ],
+            },
         ],
-        lyricists: [
-          '集体',
+        id: "fb8136b5-0b67-4906-b611-6ee7084b3fd1",
+        composers: [
+            "冼星海",
         ],
-      },
-    ],
-    id: 'fb8136b5-0b67-4906-b611-6ee7084b3fd1',
-    composers: [
-      '冼星海',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
