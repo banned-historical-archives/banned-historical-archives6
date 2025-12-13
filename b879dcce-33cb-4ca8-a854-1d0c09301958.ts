dcf7e5851a64406b50e3003c0ea3bb9e773759f8
Your Name <you@example.com>
2025-12-14 06:00:09 +0800

    1

--- b879dcce-33cb-4ca8-a854-1d0c09301958.ts
+++ b879dcce-33cb-4ca8-a854-1d0c09301958.ts
@@ -1,27 +1,39 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "b879dcce-33cb-4ca8-a854-1d0c09301958",
-        "name": "林彪、孔老二都是坏东西",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "b879dcce-33cb-4ca8-a854-1d0c09301958",
+        name: "林彪、孔老二都是坏东西",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "\n\n叛徒林彪 孔老二 \n都是坏东西 \n嘴上讲仁义 \n肚里藏鬼计 \n鼓吹克己复礼 \n一心想复辟 嘿 \n红小兵 齐上阵 \n大家都来狠狠批 \n红小兵 齐上阵 \n大家都来狠狠批 嘿 \n叛徒林彪 孔老二 \n都是坏东西 \n嘴上讲仁义 \n肚里藏鬼计 \n鼓吹克己复礼 \n一心想复辟 嘿 \n红小兵 齐上阵 \n大家都来狠狠批 \n红小兵 齐上阵 \n大家都来狠狠批 嘿\n\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/林彪、孔老二都是坏东西.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/林彪、孔老二都是坏东西.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/lbkl2dshdx.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "童声合唱佚名",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "童声合唱佚名",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
