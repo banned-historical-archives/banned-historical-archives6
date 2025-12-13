bd8f945a806d4cda872882769c8edc622e0a2cb5
Your Name <you@example.com>
2025-12-14 04:42:43 +0800

    wip

--- 52bad20d-edc8-4dae-8164-bd991761b69b.ts
+++ 52bad20d-edc8-4dae-8164-bd991761b69b.ts
@@ -1,33 +1,47 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '52bad20d-edc8-4dae-8164-bd991761b69b',
-    name: '“三、七”批示',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl47 “三、七”批示.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '“三、七”批示',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "52bad20d-edc8-4dae-8164-bd991761b69b",
+        name: "日夜想念毛主席",
+        description: "",
+        lyrics: [
+            {
+                content: "毛主席呀毛主席\n怎样才能报答您\n毛主席呀毛主席\n怎样才能报答您\n我要勤生产多卖力\n把那盘缠来攒起耶\n有一天我去看您 \n我就说毛主席耶\n普天下的人民都爱您耶\n萨拉姆毛主席\n哎 萨拉姆毛主席耶\n萨拉姆毛主席\n毛主席呀毛主席\n日夜都在想念您\n毛主席呀毛主席\n日夜都在想念您\n今天晚上我就要\n骑上毛驴去看您耶\n到了北京见到您\n我就说毛主席耶\n普天下的人民都爱您耶\n萨拉姆毛主席\n哎 萨拉姆毛主席耶\n萨拉姆毛主席\n毛主席呀毛主席\n明天就要见到您\n毛主席呀毛主席\n明天就要见到您\n把那礼物送给您\n心里话儿对您提耶\n救星毛主席\n恩人毛主席耶\n普天下的人民都爱您耶\n萨拉姆毛主席\n哎 萨拉姆毛主席耶\n萨拉姆毛主席\n萨拉姆毛主席哎\n萨拉姆毛主席\n",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/riyexiangnianmaozhuxi_2.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/riyexiangnianmaozhuxi.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "张汝襄",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "陈书斋",
+                    "曹起",
+                ],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [
+            "艾依",
+            "尼丁",
+        ],
+    },
 };
