c20b5e98b437175156b214755fb8a2fe0a20117a
Your Name <you@example.com>
2025-12-14 05:58:52 +0800

    1

--- 616e17b9-cce7-4ac7-8a77-d0dcba6610f2.ts
+++ 616e17b9-cce7-4ac7-8a77-d0dcba6610f2.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '歌唱我们的新西藏',
-    description: '',
-    lyrics: [
-      {
-        content: '雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n啊  哎  啊  哎\n牛羊肥壮遍草滩\n麦浪深处有工厂 \n大寨红花边疆开\n胜利歌声传四方\n啊  哎  啊  哎\n人民公社放射出万丈光芒\n我们前进在\n社会主义大道上\n啊  哎  啊  哎\n文化大革命结硕果\n一代新人在成长\n幸福不忘毛主席\n翻身不忘共产党\n啊  哎  啊  哎\n毛主席  共产党\n心中的红太阳\n给我们指引着胜利的方向\n雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n哎\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/gcwmdxxz.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '李文章、韩文君',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "歌唱我们的新西藏",
+        description: "",
+        lyrics: [
+            {
+                content: "雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n啊  哎  啊  哎\n牛羊肥壮遍草滩\n麦浪深处有工厂 \n大寨红花边疆开\n胜利歌声传四方\n啊  哎  啊  哎\n人民公社放射出万丈光芒\n我们前进在\n社会主义大道上\n啊  哎  啊  哎\n文化大革命结硕果\n一代新人在成长\n幸福不忘毛主席\n翻身不忘共产党\n啊  哎  啊  哎\n毛主席  共产党\n心中的红太阳\n给我们指引着胜利的方向\n雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n雪山冰峰升起红太阳\n万里高原一片灿烂阳光\n藏族人民纵情歌唱\n歌唱我们社会主义新西藏\n哎\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/gcwmdxxz.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "李文章、韩文君",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/歌唱我们的新西藏.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "王受远",
+                ],
+            },
         ],
-        lyricists: [
-          '王受远',
+        id: "616e17b9-cce7-4ac7-8a77-d0dcba6610f2",
+        composers: [
+            "夏康",
         ],
-      },
-    ],
-    id: '616e17b9-cce7-4ac7-8a77-d0dcba6610f2',
-    composers: [
-      '夏康',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
