f3cb5c816fbde9daa39260972002aa23218d1b1d
Your Name <you@example.com>
2025-12-14 05:51:30 +0800

    1

--- 1db4a212-264d-423c-99ab-6e365d39b18d.ts
+++ 1db4a212-264d-423c-99ab-6e365d39b18d.ts
@@ -1,33 +1,46 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '1db4a212-264d-423c-99ab-6e365d39b18d',
-    name: '毛主席的光辉',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/e041毛主席的光辉 北京市西城区部分小学、中学.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '北京市西城区部分小学、中学',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "1db4a212-264d-423c-99ab-6e365d39b18d",
+        name: "毛主席的光辉",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/e041毛主席的光辉 北京市西城区部分小学、中学.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "北京市西城区部分小学、中学",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/572毛主席的光辉 才旦卓玛.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "才旦卓玛",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
