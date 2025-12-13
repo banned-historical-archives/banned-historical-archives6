660802039d719980de58da3a4ea6052127df8c16
Your Name <you@example.com>
2025-12-14 05:52:49 +0800

    1

--- 3ca3b1ee-d01c-4dd6-81c1-09ce520fa620.ts
+++ 3ca3b1ee-d01c-4dd6-81c1-09ce520fa620.ts
@@ -1,32 +1,45 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '毛主席率领我们反潮流',
-    description: '',
-    lyrics: [
-      {
-        content: '举红旗 向前走\n毛主席率领我们反潮流\n反复辟 反倒退\n勇往直前不回头\n打击帝 修 反\n批判封 资 修\n迎着风浪去战斗\n继续革命不停留\n举红旗 向前走\n毛主席率领我们反潮流\n反复辟 反倒退\n勇往直前不回头\n打击帝 修 反\n批判封 资 修\n迎着风浪去战斗\n继续革命不停留\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/mzhxshlwmfchl.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '上海乐团',
-                type: '合唱团',
-              },
-            ],
-            art_forms: [
-              '合唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "毛主席率领我们反潮流",
+        description: "",
+        lyrics: [
+            {
+                content: "举红旗 向前走\n毛主席率领我们反潮流\n反复辟 反倒退\n勇往直前不回头\n打击帝 修 反\n批判封 资 修\n迎着风浪去战斗\n继续革命不停留\n举红旗 向前走\n毛主席率领我们反潮流\n反复辟 反倒退\n勇往直前不回头\n打击帝 修 反\n批判封 资 修\n迎着风浪去战斗\n继续革命不停留\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/mzhxshlwmfchl.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "上海乐团",
+                                type: "合唱团",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/490毛主席率领我们反潮流 上海乐团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "上海乐团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: '3ca3b1ee-d01c-4dd6-81c1-09ce520fa620',
-    composers: [],
-  },
-  version: 2,
+        id: "3ca3b1ee-d01c-4dd6-81c1-09ce520fa620",
+        composers: [],
+    },
+    version: 2,
 };
