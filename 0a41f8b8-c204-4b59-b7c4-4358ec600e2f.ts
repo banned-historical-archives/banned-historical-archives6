69963fc03738fa0be46ef089d2ce24de0ebb1351
Your Name <you@example.com>
2025-12-14 06:07:08 +0800

    1

--- 0a41f8b8-c204-4b59-b7c4-4358ec600e2f.ts
+++ 0a41f8b8-c204-4b59-b7c4-4358ec600e2f.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '挑担茶叶上北京',
-    description: '',
-    lyrics: [
-      {
-        content: '桑木扁担轻又轻哎\n我挑担茶叶出洞庭\n船家他问我是哪来的客哟\n我湘江边上种茶人\n桑木扁担轻又轻哎\n头上喜鹊唱不停哎\n我问喜鹊你唱什么哟\n他说我是哎 幸福人噢呵\n桑木扁担轻又轻哎\n一片茶叶一片心哎\n船家问我哪里去哟\n北京城里哎探亲人咧\n桑木扁担轻又轻\n茶叶献给毛主席\n你要问我是哪一个\n毛主席的故乡人噢\n毛主席的故乡人 哟\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/tiaodanchayeshangbeijing.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '方应暄',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "挑担茶叶上北京",
+        description: "",
+        lyrics: [
+            {
+                content: "桑木扁担轻又轻哎\n我挑担茶叶出洞庭\n船家他问我是哪来的客哟\n我湘江边上种茶人\n桑木扁担轻又轻哎\n头上喜鹊唱不停哎\n我问喜鹊你唱什么哟\n他说我是哎 幸福人噢呵\n桑木扁担轻又轻哎\n一片茶叶一片心哎\n船家问我哪里去哟\n北京城里哎探亲人咧\n桑木扁担轻又轻\n茶叶献给毛主席\n你要问我是哪一个\n毛主席的故乡人噢\n毛主席的故乡人 哟\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/tiaodanchayeshangbeijing.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "方应暄",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/挑担茶叶上北京.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "叶蔚林",
+                ],
+            },
         ],
-        lyricists: [
-          '叶蔚林',
+        id: "0a41f8b8-c204-4b59-b7c4-4358ec600e2f",
+        composers: [
+            "白诚仁",
         ],
-      },
-    ],
-    id: '0a41f8b8-c204-4b59-b7c4-4358ec600e2f',
-    composers: [
-      '白诚仁',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
