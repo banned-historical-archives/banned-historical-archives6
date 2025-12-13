307b94b37b7f0439858644cb779bba1c969b8d56
Your Name <you@example.com>
2025-12-14 03:43:34 +0800

    1

--- 3c6fb1c1-09da-4ed5-a88f-025794864a9b.ts
+++ 3c6fb1c1-09da-4ed5-a88f-025794864a9b.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '革命故事会',
-    description: '',
-    lyrics: [
-      {
-        content: '小弟弟 小妹妹\n大家来开故事会\n你讲金训华 我讲董存瑞\n雷锋 王杰 杨子荣\n英雄事迹放光辉\n讲故事 学英雄\n永远前进不掉队\n永远前进不掉队\n\n小弟弟 小妹妹\n大家来开故事会\n你讲金训华 我讲董存瑞\n雷锋 王杰 杨子荣\n英雄事迹放光辉\n讲故事 学英雄\n永远前进不掉队\n永远前进不掉队\n\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/geminggushihui.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '',
-                type: '合唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "革命故事会",
+        description: "",
+        lyrics: [
+            {
+                content: "小弟弟 小妹妹\n大家来开故事会\n你讲金训华 我讲董存瑞\n雷锋 王杰 杨子荣\n英雄事迹放光辉\n讲故事 学英雄\n永远前进不掉队\n永远前进不掉队\n\n小弟弟 小妹妹\n大家来开故事会\n你讲金训华 我讲董存瑞\n雷锋 王杰 杨子荣\n英雄事迹放光辉\n讲故事 学英雄\n永远前进不掉队\n永远前进不掉队\n\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/geminggushihui.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "",
+                                type: "合唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/革命故事会.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "马开元",
+                ],
+            },
         ],
-        lyricists: [
-          '马开元',
+        id: "3c6fb1c1-09da-4ed5-a88f-025794864a9b",
+        composers: [
+            "申以肃",
         ],
-      },
-    ],
-    id: '3c6fb1c1-09da-4ed5-a88f-025794864a9b',
-    composers: [
-      '申以肃',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
