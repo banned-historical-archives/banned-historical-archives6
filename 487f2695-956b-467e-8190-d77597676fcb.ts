64b76bcd7004ae74fc7018addd1a85ed8f47215f
Your Name <you@example.com>
2025-12-14 06:06:33 +0800

    1

--- 487f2695-956b-467e-8190-d77597676fcb.ts
+++ 487f2695-956b-467e-8190-d77597676fcb.ts
@@ -1,27 +1,48 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "487f2695-956b-467e-8190-d77597676fcb",
-        "name": "政治夜校亮堂堂",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "487f2695-956b-467e-8190-d77597676fcb",
+        name: "政治夜校亮堂堂",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "明月照山岗哎 满村灯火亮\n歌声阵阵起 一片新气象\n哎嗨嗨 一片新气象\n贫下中农上夜校\n政治夜校亮堂堂\n贫下中农上夜校\n政治夜校亮堂堂\n政治夜校亮堂堂\n批林批孔火正旺\n老支书登台讲村史\n大批判文章贴满墙\n毛主席著作认真学\n基本路线天天讲\n上层建筑插红旗\n红色江山万年长\n毛主席的光辉哎 照山乡\n政治夜校建在咱的心坎上\n明月照山岗哎 满村灯火亮\n歌声阵阵起 一片新气象\n哎嗨嗨 一片新气象\n贫下中农上夜校\n政治夜校威力强\n贫下中农上夜校\n政治夜校威力强\n政治夜校威力强\n贫下中农豪情壮\n社员们攀登理论山\n学习马列主义心头亮\n无产阶级专政更巩固\n社会主义道路多宽广\n学习大寨争上游\n备战备荒广积粮\n毛主席的光辉哎 照山乡\n政治夜校亮堂堂\n毛主席的光辉哎 照山乡\n政治夜校亮堂堂嘿亮堂堂\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/政治夜校亮堂堂.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/政治夜校亮堂堂.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zhengzhiyexiaoliangtangtang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "吉林省歌舞团",
+                                type: "合唱团",
+                            },
+                            {
+                                name: "张培兰",
+                                type: "领唱",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "孟令政",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "赵云程",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
