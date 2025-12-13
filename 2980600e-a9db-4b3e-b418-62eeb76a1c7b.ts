e4570f172ebe5b5fcc8989c154e1ef14417644cf
Your Name <you@example.com>
2025-12-14 04:27:00 +0800

    wip

--- 2980600e-a9db-4b3e-b418-62eeb76a1c7b.ts
+++ 2980600e-a9db-4b3e-b418-62eeb76a1c7b.ts
@@ -30,6 +30,14 @@ export default {
                             "独唱",
                         ],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl85xb领导我们事业的核心力量.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
                 lyricists: [],
             },
