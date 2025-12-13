e46f6d656dcad842a017b6aa55d8ccd7c89fe15d
Your Name <you@example.com>
2025-12-14 05:39:48 +0800

    1

--- 98b22367-9f2d-414b-a1e8-f67f33e16cc4.ts
+++ 98b22367-9f2d-414b-a1e8-f67f33e16cc4.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '畲家歌颂毛主席',
-    description: '',
-    lyrics: [
-      {
-        content: '\n出山泉水清又清哎啰\n饮口清泉甜透心唻\n幸福清泉谁引来哎\n伟大领袖毛主席啰\n冲破长夜到天明哎\n红旗插上畲山岭哎\n奴隶翻身哩做主人唻\n畲家人民喜迎春啰\n饮口啊清泉哩甜透心唻\n毛主席是咱引路人啰\n金光大道党指引哎\n大寨花开畲山顶哎\n稻谷飘香哩茶林茂哇\n山山水水一片新啰\n畲家啊年年哩夺丰收唻\n毛主席是咱引路人啰\n文化大革命凯歌传哎\n批林批孔战旗红哎\n基本路线哩记在心唻\n反修防修筑长城啰\n祖国江山哩万年红唻\n毛主席是咱引路人啰\n山上青松根连根哎啰\n畲家和党心连心唻\n畲山顶上放声唱哎\n万岁万岁毛主席啰\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/shejiagesongmaozhuxi.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '施爱华领唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "畲家歌颂毛主席",
+        description: "",
+        lyrics: [
+            {
+                content: "\n出山泉水清又清哎啰\n饮口清泉甜透心唻\n幸福清泉谁引来哎\n伟大领袖毛主席啰\n冲破长夜到天明哎\n红旗插上畲山岭哎\n奴隶翻身哩做主人唻\n畲家人民喜迎春啰\n饮口啊清泉哩甜透心唻\n毛主席是咱引路人啰\n金光大道党指引哎\n大寨花开畲山顶哎\n稻谷飘香哩茶林茂哇\n山山水水一片新啰\n畲家啊年年哩夺丰收唻\n毛主席是咱引路人啰\n文化大革命凯歌传哎\n批林批孔战旗红哎\n基本路线哩记在心唻\n反修防修筑长城啰\n祖国江山哩万年红唻\n毛主席是咱引路人啰\n山上青松根连根哎啰\n畲家和党心连心唻\n畲山顶上放声唱哎\n万岁万岁毛主席啰\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/shejiagesongmaozhuxi.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "施爱华领唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/畲家歌颂毛主席.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "蓝天",
+                ],
+            },
         ],
-        lyricists: [
-          '蓝天',
+        id: "98b22367-9f2d-414b-a1e8-f67f33e16cc4",
+        composers: [
+            "献芝",
         ],
-      },
-    ],
-    id: '98b22367-9f2d-414b-a1e8-f67f33e16cc4',
-    composers: [
-      '献芝',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
--- d329c96a-98ba-41e8-8f6e-1b3315158f90.ts
+++ d329c96a-98ba-41e8-8f6e-1b3315158f90.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'd329c96a-98ba-41e8-8f6e-1b3315158f90',
-    name: '真正的朋友',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl92真正的朋友.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "d329c96a-98ba-41e8-8f6e-1b3315158f90",
+        name: "真正的朋友",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl92真正的朋友.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl92a真正的朋友.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
