68235992d79c2c945102e5e8cb80877ed345ad97
Your Name <you@example.com>
2025-12-14 05:44:57 +0800

    1

--- 237b27d5-12c1-48e6-9317-422ed3eb3d15.ts
+++ 237b27d5-12c1-48e6-9317-422ed3eb3d15.ts
@@ -1,32 +1,45 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '永远歌唱伟大领袖毛主席',
-    description: '',
-    lyrics: [
-      {
-        content: '千山万水回荡着东方红的歌声\n千秋万代铭记着毛主席的恩情\n毛主席开辟了革命的道路\n盼望解放的人民从此得到新生\n毛主席您是不落的太阳\n毛主席您永远活在我们心中\n我们世世代代放声歌唱\n歌唱敬爱的领袖毛泽东\n我们世世代代放声歌唱\n歌唱敬爱的领袖毛泽东\n文化大革命推动着历史的车轮\n反修防修巩固无产阶级专政\n毛主席是当代最伟大的\n马克思主义者\n光辉的思想照亮继续革命航程\n毛主席您是不落的太阳\n毛主席您永远活在我们心中\n我们世世代代放声歌唱\n歌唱伟大的导师毛泽东\n我们世世代代放声歌唱\n歌唱伟大的导师毛泽东\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/yygchwdlxmzhx.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '赵士静、李维领唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "永远歌唱伟大领袖毛主席",
+        description: "",
+        lyrics: [
+            {
+                content: "千山万水回荡着东方红的歌声\n千秋万代铭记着毛主席的恩情\n毛主席开辟了革命的道路\n盼望解放的人民从此得到新生\n毛主席您是不落的太阳\n毛主席您永远活在我们心中\n我们世世代代放声歌唱\n歌唱敬爱的领袖毛泽东\n我们世世代代放声歌唱\n歌唱敬爱的领袖毛泽东\n文化大革命推动着历史的车轮\n反修防修巩固无产阶级专政\n毛主席是当代最伟大的\n马克思主义者\n光辉的思想照亮继续革命航程\n毛主席您是不落的太阳\n毛主席您永远活在我们心中\n我们世世代代放声歌唱\n歌唱伟大的导师毛泽东\n我们世世代代放声歌唱\n歌唱伟大的导师毛泽东\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/yygchwdlxmzhx.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "赵士静、李维领唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/608永远歌唱伟大领袖毛主席 沈阳部队政治部歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "沈阳部队政治部歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: '237b27d5-12c1-48e6-9317-422ed3eb3d15',
-    composers: [],
-  },
-  version: 2,
+        id: "237b27d5-12c1-48e6-9317-422ed3eb3d15",
+        composers: [],
+    },
+    version: 2,
 };
