77dfc390fd52645ef8a349145d41887444a4b3b2
Your Name <you@example.com>
2025-12-14 05:38:53 +0800

    1

--- d2336602-8d93-4d7b-97a9-aa90b95e1b27.ts
+++ d2336602-8d93-4d7b-97a9-aa90b95e1b27.ts
@@ -1,41 +1,49 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '秋收起义歌',
-    description: '',
-    lyrics: [
-      {
-        content: '霹雳一声震天响\n秋收起义举刀枪\n跟着领袖毛委员\n砸碎镣铐求解放\n工友农友团结一条心哎\n手中的枪杆要握紧\n咳 要握紧 咳\n革命永远向前进\n一杆大旗迎风扬\n镰刀斧头闪金光\n跟着领袖毛委员\n浩浩荡荡上井冈\n工友农友团结一条心哎\n根据地建立在农村\n咳 在农村 咳\n革命永远向前进 向前进\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/qiusouqiyige.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '合唱',
-                type: '独唱',
-              },
-              {
-                name: '张士燮',
-                type: '独唱',
-              },
-              {
-                name: '陈杰整理',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-              '湖南民歌',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "秋收起义歌",
+        description: "",
+        lyrics: [
+            {
+                content: "霹雳一声震天响\n秋收起义举刀枪\n跟着领袖毛委员\n砸碎镣铐求解放\n工友农友团结一条心哎\n手中的枪杆要握紧\n咳 要握紧 咳\n革命永远向前进\n一杆大旗迎风扬\n镰刀斧头闪金光\n跟着领袖毛委员\n浩浩荡荡上井冈\n工友农友团结一条心哎\n根据地建立在农村\n咳 在农村 咳\n革命永远向前进 向前进\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/qiusouqiyige.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                            {
+                                name: "张士燮",
+                                type: "独唱",
+                            },
+                            {
+                                name: "陈杰整理",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                            "湖南民歌",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/秋收起义歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: 'd2336602-8d93-4d7b-97a9-aa90b95e1b27',
-    composers: [],
-  },
-  version: 2,
+        id: "d2336602-8d93-4d7b-97a9-aa90b95e1b27",
+        composers: [],
+    },
+    version: 2,
 };
