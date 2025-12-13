2307c56c54861f91ecb45a0fdd2eb9bf142d01e3
Your Name <you@example.com>
2025-12-14 04:48:58 +0800

    1

--- 4c7fa01b-f9a0-4efd-a6b5-e283fd88d67b.ts
+++ 4c7fa01b-f9a0-4efd-a6b5-e283fd88d67b.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '雄伟的天安门',
-    description: '',
-    lyrics: [
-      {
-        content: '雄伟的天安门 雄伟的广场\n第一面五星红旗升起的地方\n打从这面战旗在这里升起\n中国人民的心中\n从此充满阳光 从此充满阳光\n雄伟的天安门 宽阔的广场\n毛主席检阅革命队伍的地方\n每当我们从这里走过\n便会感到胸怀宽阔\n胜过海洋 胜过海洋\n雄伟的天安门 壮丽的广场\n各族人民衷心景仰的地方\n虽然我们住在祖国各地\n颗颗红心都朝着这个方向\n都朝着这个方向\n都朝着这个方向\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xiongweidetiananmen.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '张振富',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "雄伟的天安门",
+        description: "",
+        lyrics: [
+            {
+                content: "雄伟的天安门 雄伟的广场\n第一面五星红旗升起的地方\n打从这面战旗在这里升起\n中国人民的心中\n从此充满阳光 从此充满阳光\n雄伟的天安门 宽阔的广场\n毛主席检阅革命队伍的地方\n每当我们从这里走过\n便会感到胸怀宽阔\n胜过海洋 胜过海洋\n雄伟的天安门 壮丽的广场\n各族人民衷心景仰的地方\n虽然我们住在祖国各地\n颗颗红心都朝着这个方向\n都朝着这个方向\n都朝着这个方向\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xiongweidetiananmen.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "张振富",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/雄伟的天安门.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "乔羽",
+                ],
+            },
         ],
-        lyricists: [
-          '乔羽',
+        id: "4c7fa01b-f9a0-4efd-a6b5-e283fd88d67b",
+        composers: [
+            "梁克祥",
         ],
-      },
-    ],
-    id: '4c7fa01b-f9a0-4efd-a6b5-e283fd88d67b',
-    composers: [
-      '梁克祥',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
