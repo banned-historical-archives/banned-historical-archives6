2d346f8cba4fd654be3639db6c920bca82234e69
Your Name <you@example.com>
2025-12-14 06:10:30 +0800

    1

--- c8b99cf6-65ea-4afb-ad29-e07fca0ed7a1.ts
+++ c8b99cf6-65ea-4afb-ad29-e07fca0ed7a1.ts
@@ -1,27 +1,48 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "c8b99cf6-65ea-4afb-ad29-e07fca0ed7a1",
-        "name": "我心中的歌献给解放军",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "c8b99cf6-65ea-4afb-ad29-e07fca0ed7a1",
+        name: "我心中的歌献给解放军",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "不敬青稞酒呀\n不打酥油茶呀 也不献哈达\n唱上一支心中的歌儿\n献给亲人金珠玛\n梭 牙拉梭\n献给亲人金珠玛\n感谢你们帮我们闹翻身哎\n百万农奴当家作主人哎\n感谢你们支左支工又支农\n文化大革命立新功\n立呀立新功哎\n感谢你们带来了\n毛主席的书哎\n革命真理永远记心中哎\n感谢你们紧握枪杆保边疆\n人民的江山万年红\n万呀万年红哎\n不敬青稞酒呀\n不打酥油茶呀 也不献哈达\n唱上一支心中的歌儿\n献给亲人金珠玛\n梭 牙拉梭\n献给亲人金珠玛\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我心中的歌献给解放军.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我心中的歌献给解放军.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/wxzhdgxgjfj.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "张振富",
+                                type: "独唱",
+                            },
+                            {
+                                name: "常留柱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "庄涛",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "常留柱",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
