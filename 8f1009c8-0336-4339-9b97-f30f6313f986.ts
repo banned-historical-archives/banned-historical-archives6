b97a328ea26204135009b16b0264fe4d0a59cf17
Your Name <you@example.com>
2025-12-14 05:14:08 +0800

    1

--- 8f1009c8-0336-4339-9b97-f30f6313f986.ts
+++ 8f1009c8-0336-4339-9b97-f30f6313f986.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "8f1009c8-0336-4339-9b97-f30f6313f986",
-        "name": "革命战士最光荣",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "8f1009c8-0336-4339-9b97-f30f6313f986",
+        name: "革命战士最光荣",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/革命战士最光荣.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/革命战士最光荣.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/284革命战士最光荣 江西省歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "江西省歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
