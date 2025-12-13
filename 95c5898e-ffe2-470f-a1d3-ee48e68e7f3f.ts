0c4db2f10d6521c104b2cb3ce6d402d27383d543
Your Name <you@example.com>
2025-12-14 05:24:58 +0800

    1

--- 95c5898e-ffe2-470f-a1d3-ee48e68e7f3f.ts
+++ 95c5898e-ffe2-470f-a1d3-ee48e68e7f3f.ts
@@ -3,7 +3,7 @@ export default {
   version: 2,
   entity: {
     id: '95c5898e-ffe2-470f-a1d3-ee48e68e7f3f',
-    name: '解放军学全国人民',
+    name: '解放军学全国人民(小众版本)',
     description: '',
     lyrics: [
       {
