ada01633d7b2be099d5398db81131b5656fe26d8
Your Name <you@example.com>
2025-12-14 04:56:15 +0800

    1

--- 9addf799-c507-41cb-83be-b6d4abdbbc8e.ts
+++ 9addf799-c507-41cb-83be-b6d4abdbbc8e.ts
@@ -1,36 +1,49 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '我爱北京天安门',
-    description: '',
-    lyrics: [
-      {
-        content: '我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/woaibeijintiananmen.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '童声合唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "我爱北京天安门",
+        description: "",
+        lyrics: [
+            {
+                content: "我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n我爱北京天安门\n天安门上太阳升\n伟大领袖毛主席\n指引我们向前进\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/woaibeijintiananmen.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "童声合唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/e001我爱北京天安门 北京市八校学生.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "北京市八校学生",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "金果临",
+                ],
+            },
         ],
-        lyricists: [
-          '金果临',
+        id: "9addf799-c507-41cb-83be-b6d4abdbbc8e",
+        composers: [
+            "金月苓",
         ],
-      },
-    ],
-    id: '9addf799-c507-41cb-83be-b6d4abdbbc8e',
-    composers: [
-      '金月苓',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
