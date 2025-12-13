50b744b2c68602e85bd0dc58b4e704c6a5d251c1
Your Name <you@example.com>
2025-12-14 05:56:56 +0800

    1

--- bed4a4b1-894d-4052-a55c-cc6a8dcce0a9.ts
+++ bed4a4b1-894d-4052-a55c-cc6a8dcce0a9.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '毕业歌',
-    description: '',
-    lyrics: [
-      {
-        content: '同学们 大家起来\n        奔向那抗战的前方\n        听吧 抗战的号角已吹响\n        看吧 战斗的红旗在飘扬\n        我们跟着共产党拿起枪\n        我们誓死保卫祖国的边疆\n        我们决心把侵略者\n        彻底埋葬\n        我们要和工农在一起\n        筑成那铁壁铜墙\n        全国人民团结起来\n        迎接那民族解放\n        胜利的曙光\n        前进 前进 军号已吹响\n        同学们 同学们\n        快行动起来\n        奔向那抗战的前方\n        前进 前进 军号已吹响\n        同学们 同学们\n        快行动起来\n        奔向那抗战的前方',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/biyege.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中央乐团',
-                type: '合唱团',
-              },
-            ],
-            art_forms: [
-              '合唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "毕业歌",
+        description: "",
+        lyrics: [
+            {
+                content: "同学们 大家起来\n        奔向那抗战的前方\n        听吧 抗战的号角已吹响\n        看吧 战斗的红旗在飘扬\n        我们跟着共产党拿起枪\n        我们誓死保卫祖国的边疆\n        我们决心把侵略者\n        彻底埋葬\n        我们要和工农在一起\n        筑成那铁壁铜墙\n        全国人民团结起来\n        迎接那民族解放\n        胜利的曙光\n        前进 前进 军号已吹响\n        同学们 同学们\n        快行动起来\n        奔向那抗战的前方\n        前进 前进 军号已吹响\n        同学们 同学们\n        快行动起来\n        奔向那抗战的前方",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/biyege.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央乐团",
+                                type: "合唱团",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毕业歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "中央乐团",
+                ],
+            },
         ],
-        lyricists: [
-          '中央乐团',
+        id: "bed4a4b1-894d-4052-a55c-cc6a8dcce0a9",
+        composers: [
+            "聂耳",
         ],
-      },
-    ],
-    id: 'bed4a4b1-894d-4052-a55c-cc6a8dcce0a9',
-    composers: [
-      '聂耳',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
