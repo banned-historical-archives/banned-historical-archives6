b1ee096078425b6c22930e9d0ea4099b6756fc4f
Your Name <you@example.com>
2025-12-14 05:50:31 +0800

    1

--- d3634c6a-3e22-4309-80ff-46c386895ebe.ts
+++ d3634c6a-3e22-4309-80ff-46c386895ebe.ts
@@ -1,28 +1,45 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'd3634c6a-3e22-4309-80ff-46c386895ebe',
-    name: '毛主席的恩情比山高比水长',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/794毛主席的恩情比山高比水长.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "d3634c6a-3e22-4309-80ff-46c386895ebe",
+        name: "毛主席的恩情比山高比水长",
+        description: "",
+        lyrics: [
+            {
+                content: "巍巍井冈山高\n滚滚延河水长\n毛主席的恩情\n比山高比水长\n啊 毛主席的恩情\n比山高比水长\n天安门红旗飘\n中南海明灯亮\n赞颂毛主席的歌\n千代传万代唱\n啊 赞颂毛主席的歌\n千代传万代唱\n赞颂毛主席的歌\n千代传万代唱\n",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/794毛主席的恩情比山高比水长.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/maozhuxideenqingbishangaobishuichang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "李双江",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "唐青林",
+                ],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [
+            "韩夏",
+        ],
+    },
 };
