284191b975d4eb12e33e710204de57c12be9855a
Your Name <you@example.com>
2025-12-14 05:52:13 +0800

    1

--- c0ec7d5b-4ae2-426e-a591-4d741843d7f3.ts
+++ c0ec7d5b-4ae2-426e-a591-4d741843d7f3.ts
@@ -1,33 +1,45 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'c0ec7d5b-4ae2-426e-a591-4d741843d7f3',
-    name: '毛主席率领我们奔向前方',
-    description: '',
-    lyrics: [
-      {
-        content: "红日照耀青山绿水，祖国一派大好春光。各族人民朝气蓬勃，满怀豪情放声歌唱。文化大革命春风浩荡，新生事物茁壮成长。马列主义光辉普照，批林批孔战歌嘹亮。亿万人民团结战斗，团结战斗，革命航船乘风破浪，乘风破浪。火红的太阳照征途，毛主席率领我们奔向前方，奔向前方！红日照耀青山绿水，住过一派大好春光。各族人民朝气蓬勃，满怀豪情放声歌唱。大庆红旗迎风招展，大寨红花到处飘香。社会主义胜利前进，自立更生凯歌高唱。伟大住过欣欣向荣，欣欣向荣，锦绣前程灿烂辉煌，灿烂辉煌。火红的太阳照征途，毛主席率领我们奔向前方！",
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/489毛主席率领我们奔向前方 王凯平.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '王凯平',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "c0ec7d5b-4ae2-426e-a591-4d741843d7f3",
+        name: "毛主席率领我们奔向前方",
+        description: "",
+        lyrics: [
+            {
+                content: "红日照耀青山绿水，祖国一派大好春光。各族人民朝气蓬勃，满怀豪情放声歌唱。文化大革命春风浩荡，新生事物茁壮成长。马列主义光辉普照，批林批孔战歌嘹亮。亿万人民团结战斗，团结战斗，革命航船乘风破浪，乘风破浪。火红的太阳照征途，毛主席率领我们奔向前方，奔向前方！红日照耀青山绿水，住过一派大好春光。各族人民朝气蓬勃，满怀豪情放声歌唱。大庆红旗迎风招展，大寨红花到处飘香。社会主义胜利前进，自立更生凯歌高唱。伟大住过欣欣向荣，欣欣向荣，锦绣前程灿烂辉煌，灿烂辉煌。火红的太阳照征途，毛主席率领我们奔向前方！",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/489毛主席率领我们奔向前方 王凯平.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "王凯平",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席率领我们奔向前方.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "苏伟光",
+                ],
+            },
         ],
-        lyricists: ["苏伟光"],
-      },
-    ],
-    composers: ["黄祖禧"],
-  },
+        composers: [
+            "黄祖禧",
+        ],
+    },
 };
