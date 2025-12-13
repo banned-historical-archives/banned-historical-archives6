72fe2f118939108429a32cde45ceb43ff7aa87fc
Your Name <you@example.com>
2025-12-14 05:13:10 +0800

    1

--- d0a25b99-e327-4dd3-abaf-3c53861261fb.ts
+++ d0a25b99-e327-4dd3-abaf-3c53861261fb.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "d0a25b99-e327-4dd3-abaf-3c53861261fb",
-        "name": "革命青年之歌",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "d0a25b99-e327-4dd3-abaf-3c53861261fb",
+        name: "革命青年之歌",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/革命青年之歌.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/革命青年之歌.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/541革命青年之歌 天津歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "天津歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
