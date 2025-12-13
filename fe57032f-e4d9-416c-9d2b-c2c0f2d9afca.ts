6d4faf9978e34e27ed7dbe2129f253534e16ec8b
Your Name <you@example.com>
2025-12-14 05:40:19 +0800

    1

--- fe57032f-e4d9-416c-9d2b-c2c0f2d9afca.ts
+++ fe57032f-e4d9-416c-9d2b-c2c0f2d9afca.ts
@@ -1,36 +1,49 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '生活在毛泽东时代',
-    description: '',
-    lyrics: [
-      {
-        content: '\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n革命先辈流血流汗\n才把今天幸福换来\n我们要艰苦奋斗革命到底\n回答祖国对我们的期待\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n阶级斗争永远不忘\n党的教导牢记心怀\n我们要学习雷锋光辉榜样\n创造人类最美好的未来\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/shenghuozaimaozedongshidai.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '上海广播电视艺术团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "生活在毛泽东时代",
+        description: "",
+        lyrics: [
+            {
+                content: "\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n革命先辈流血流汗\n才把今天幸福换来\n我们要艰苦奋斗革命到底\n回答祖国对我们的期待\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n阶级斗争永远不忘\n党的教导牢记心怀\n我们要学习雷锋光辉榜样\n创造人类最美好的未来\n红旗为我们引路\n鲜花为我们盛开\n谁能比我们更幸福\n生活在毛泽东的时代\n生活在毛泽东时代\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/shenghuozaimaozedongshidai.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "上海广播电视艺术团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/497生活在毛泽东时代 上海广播电视艺术团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "上海广播电视艺术团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "邬大为",
+                ],
+            },
         ],
-        lyricists: [
-          '邬大为',
+        id: "fe57032f-e4d9-416c-9d2b-c2c0f2d9afca",
+        composers: [
+            "瞿维",
         ],
-      },
-    ],
-    id: 'fe57032f-e4d9-416c-9d2b-c2c0f2d9afca',
-    composers: [
-      '瞿维',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
