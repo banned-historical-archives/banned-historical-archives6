404404f879e8d12db093e5b8ae264b2574f18468
Your Name <you@example.com>
2025-12-14 04:43:11 +0800

    wip

--- 56e0e849-1fec-4f3b-b9af-81c488f92855.ts
+++ 56e0e849-1fec-4f3b-b9af-81c488f92855.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "56e0e849-1fec-4f3b-b9af-81c488f92855",
-        "name": "向新的胜利进军",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "56e0e849-1fec-4f3b-b9af-81c488f92855",
+        name: "向新的胜利进军",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/向新的胜利进军.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/向新的胜利进军.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/722向新的胜利进军 陕西省歌舞剧院.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "陕西省歌舞剧院",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
