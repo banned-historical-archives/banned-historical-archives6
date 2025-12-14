36c9ec36934b97da9f8faa8fbf79222ca417ad59
Your Name <you@example.com>
2025-12-14 14:24:02 +0800

    1

--- 6810ef12-e4d0-4d67-989d-925334e1d691.ts
+++ 6810ef12-e4d0-4d67-989d-925334e1d691.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '6810ef12-e4d0-4d67-989d-925334e1d691',
-    name: '大港石油工人战歌',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/527大港石油工人战歌 天津市歌舞团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '天津市歌舞团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "6810ef12-e4d0-4d67-989d-925334e1d691",
+        name: "大港石油工人战歌",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/527大港石油工人战歌 天津市歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "天津市歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/大港石油工人战歌.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
