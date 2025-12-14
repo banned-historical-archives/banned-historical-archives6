54a495ff169c8271c7e76e2150c635ebdead05f0
Your Name <you@example.com>
2025-12-14 14:38:36 +0800

    1

--- a437d375-828e-4e62-8f51-3843a0b5259d.ts
+++ a437d375-828e-4e62-8f51-3843a0b5259d.ts
@@ -29,6 +29,19 @@ export default {
                         artists: [],
                         art_forms: [],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/554医疗队员之歌 中央广播文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央广播文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
                 lyricists: [],
             },
