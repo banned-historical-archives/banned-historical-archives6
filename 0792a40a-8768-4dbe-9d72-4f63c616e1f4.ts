4319c584bbdd967d69861dd2faa4860abd858112
Your Name <you@example.com>
2025-12-14 14:22:28 +0800

    1

--- 0792a40a-8768-4dbe-9d72-4f63c616e1f4.ts
+++ 0792a40a-8768-4dbe-9d72-4f63c616e1f4.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '0792a40a-8768-4dbe-9d72-4f63c616e1f4',
-    name: '学习英雄蔡永祥',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/329a学习英雄蔡永祥 战友文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '战友文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "0792a40a-8768-4dbe-9d72-4f63c616e1f4",
+        name: "学习英雄蔡永祥",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/329a学习英雄蔡永祥 战友文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "战友文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/学习英雄蔡永祥.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
