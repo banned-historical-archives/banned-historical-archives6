a87ba5abc9490f8aaf66590d3fe692866165be73
Your Name <you@example.com>
2025-12-14 13:46:18 +0800

    1

--- 1750e36d-dad5-4b0e-b7b1-ef2125af9681.ts
+++ 1750e36d-dad5-4b0e-b7b1-ef2125af9681.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '1750e36d-dad5-4b0e-b7b1-ef2125af9681',
-    name: '我们的文学艺术首先是为工农兵的',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl36x我们的文学艺术首先是为工农兵的.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "1750e36d-dad5-4b0e-b7b1-ef2125af9681",
+        name: "我们的文学艺术首先是为工农兵的",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl36x我们的文学艺术首先是为工农兵的.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl36我们的文学艺术首先是为工农兵的.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
