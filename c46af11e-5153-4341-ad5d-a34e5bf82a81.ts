3c915c828634ec046a2157c283ce6793201749f7
Your Name <you@example.com>
2025-12-14 14:31:30 +0800

    1

--- c46af11e-5153-4341-ad5d-a34e5bf82a81.ts
+++ c46af11e-5153-4341-ad5d-a34e5bf82a81.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '喜送公粮破浪来',
-    description: '',
-    lyrics: [
-      {
-        content: '\n大船开咧小船来\n送粮船队一排排\n汽笛声声催船快\n喜送公粮破浪来\n心花放咧浪花开\n金谷如山歌如海\n社社队队广积粮\n贫下中农喜满怀\n心花放 浪花开\n跃进歌声传天外\n站在船头咧放眼望啊\n锦绣河山好气派\n大寨红旗迎风摆\n千山万弄面貌改\n人民公社庆丰收啊\n党的路线放光彩\n放光彩啰放光彩\n精选好粮献国家\n座座金山河中排\n毛主席播下幸福种呀\n丰收果实万船载\n万船载啰万船载\n大船开咧小船来\n送粮船队一排排\n飞船劈开千层浪哎\n急流险滩脚下踩\n一轮红日心头照哎\n喜送公粮破浪来咧\n破浪来 破浪来\n咳啰咳咳啰咳\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xisonggongliangpolanglai.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '广西壮族自治区歌舞团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "喜送公粮破浪来",
+        description: "",
+        lyrics: [
+            {
+                content: "\n大船开咧小船来\n送粮船队一排排\n汽笛声声催船快\n喜送公粮破浪来\n心花放咧浪花开\n金谷如山歌如海\n社社队队广积粮\n贫下中农喜满怀\n心花放 浪花开\n跃进歌声传天外\n站在船头咧放眼望啊\n锦绣河山好气派\n大寨红旗迎风摆\n千山万弄面貌改\n人民公社庆丰收啊\n党的路线放光彩\n放光彩啰放光彩\n精选好粮献国家\n座座金山河中排\n毛主席播下幸福种呀\n丰收果实万船载\n万船载啰万船载\n大船开咧小船来\n送粮船队一排排\n飞船劈开千层浪哎\n急流险滩脚下踩\n一轮红日心头照哎\n喜送公粮破浪来咧\n破浪来 破浪来\n咳啰咳咳啰咳\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xisonggongliangpolanglai.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "广西壮族自治区歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/喜送公粮破浪来.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "永祥、罗湜",
+                ],
+            },
         ],
-        lyricists: [
-          '永祥、罗湜',
+        id: "c46af11e-5153-4341-ad5d-a34e5bf82a81",
+        composers: [
+            "林雨谷",
         ],
-      },
-    ],
-    id: 'c46af11e-5153-4341-ad5d-a34e5bf82a81',
-    composers: [
-      '林雨谷',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
