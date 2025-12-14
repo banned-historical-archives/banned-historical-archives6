e47ce57b2c40c877669b91ac2ca84883e84e71fd
Your Name <you@example.com>
2025-12-14 14:32:45 +0800

    1

--- d7dc20ef-5935-48d6-bfda-64c6d82b532f.ts
+++ d7dc20ef-5935-48d6-bfda-64c6d82b532f.ts
@@ -1,27 +1,44 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "d7dc20ef-5935-48d6-bfda-64c6d82b532f",
-        "name": "哈尼人民热爱毛主席",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "d7dc20ef-5935-48d6-bfda-64c6d82b532f",
+        name: "哈尼人民热爱毛主席",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "火红的马樱花\n最喜欢及时的春雨\n金黄的向日葵花\n最喜欢阳光的抚育\n青青的禾苗\n最喜欢温暖的春风\n边疆的哈尼人民哎\n最热爱领袖毛主席\n最热爱领袖毛主席\n毛主席号召我们学大寨\n梯田层层云中起\n毛主席号召我们学大庆\n矿山处处飘红旗\n毛主席教导我们团结紧\n各族人民心连心\n毛主席领导我们向前进\n誓将革命进行到底\n誓将革命进行到底\n进行到底\n火红的马樱花\n最喜欢及时的春雨\n金黄的向日葵花\n最喜欢阳光的抚育\n青青的禾苗\n最喜欢温暖的春风\n边疆的哈尼人民哎\n最热爱领袖毛主席\n最热爱领袖毛主席 毛主席\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/哈尼人民热爱毛主席.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/哈尼人民热爱毛主席.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/hnrmramzx.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "杜丽华",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "包钢",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "梅璧",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
