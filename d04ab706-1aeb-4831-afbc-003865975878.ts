8dd9c2df8ee622a54770150589054b33010738ef
Your Name <you@example.com>
2025-12-14 14:29:37 +0800

    1

--- d04ab706-1aeb-4831-afbc-003865975878.ts
+++ d04ab706-1aeb-4831-afbc-003865975878.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '壮家少年热爱毛主席',
-    description: '',
-    lyrics: [
-      {
-        content: '\n红艳艳的木棉在南疆开放\n壮家的少年在红旗下成长\n教室里 操场上\n好好学习 天天向上\n工厂里 田野上\n学工学农歌声嘹亮\n啊……\n壮家的少年像春天的花朵\n像茁壮的秧苗\n沐浴着灿烂的阳光\n\n红艳艳的木棉在南疆开放\n壮家的少年在红旗下成长\n学政治 学文化\n革命本领党培养\n心儿红 身体壮\n朝气蓬勃有理想\n啊……\n壮家的少年在祖国的怀抱\n在美丽的壮乡\n肩负着人民的希望\n\n木棉花迎春风\n红领巾披霞光\n壮家少年\n颗颗心儿向着红太阳\n毛主席啊毛主席\n壮家少年\n在您的关怀下幸福成长\n\n木棉花迎春风\n红领巾披霞光\n壮家少年\n颗颗心儿向着红太阳\n毛主席啊毛主席\n壮家少年\n在您的关怀下幸福成长\n在您的关怀下幸福成长\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zhuangjiashaonianreaimaozhuxi.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '央广少年广播合唱团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "壮家少年热爱毛主席",
+        description: "",
+        lyrics: [
+            {
+                content: "\n红艳艳的木棉在南疆开放\n壮家的少年在红旗下成长\n教室里 操场上\n好好学习 天天向上\n工厂里 田野上\n学工学农歌声嘹亮\n啊……\n壮家的少年像春天的花朵\n像茁壮的秧苗\n沐浴着灿烂的阳光\n\n红艳艳的木棉在南疆开放\n壮家的少年在红旗下成长\n学政治 学文化\n革命本领党培养\n心儿红 身体壮\n朝气蓬勃有理想\n啊……\n壮家的少年在祖国的怀抱\n在美丽的壮乡\n肩负着人民的希望\n\n木棉花迎春风\n红领巾披霞光\n壮家少年\n颗颗心儿向着红太阳\n毛主席啊毛主席\n壮家少年\n在您的关怀下幸福成长\n\n木棉花迎春风\n红领巾披霞光\n壮家少年\n颗颗心儿向着红太阳\n毛主席啊毛主席\n壮家少年\n在您的关怀下幸福成长\n在您的关怀下幸福成长\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/zhuangjiashaonianreaimaozhuxi.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "央广少年广播合唱团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/壮家少年热爱毛主席.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "曾宪瑞",
+                ],
+            },
         ],
-        lyricists: [
-          '曾宪瑞',
+        id: "d04ab706-1aeb-4831-afbc-003865975878",
+        composers: [
+            "钟庆民",
         ],
-      },
-    ],
-    id: 'd04ab706-1aeb-4831-afbc-003865975878',
-    composers: [
-      '钟庆民',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
