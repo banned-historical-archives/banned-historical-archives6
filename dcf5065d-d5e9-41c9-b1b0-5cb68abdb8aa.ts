3ab6c3914628426d8ca5ede581c5b2a22ce2a8d3
Your Name <you@example.com>
2025-12-14 13:48:39 +0800

    1

--- dcf5065d-d5e9-41c9-b1b0-5cb68abdb8aa.ts
+++ dcf5065d-d5e9-41c9-b1b0-5cb68abdb8aa.ts
@@ -1,27 +1,53 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "dcf5065d-d5e9-41c9-b1b0-5cb68abdb8aa",
-        "name": "我们永远是个战斗队",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "dcf5065d-d5e9-41c9-b1b0-5cb68abdb8aa",
+        name: "我们永远是个战斗队",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们永远是个战斗队.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们永远是个战斗队.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/165a我们永远是个战斗队 解放军歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "解放军歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/165我们永远是个战斗队 浙江省歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "浙江省歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
