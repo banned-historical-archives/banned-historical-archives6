44051608b8b476a3e6339e4e59cc5528164b2a98
Your Name <you@example.com>
2025-12-14 13:58:54 +0800

    1

--- 760eeff5-684f-4d20-9114-86f0173ccc2e.ts
+++ 760eeff5-684f-4d20-9114-86f0173ccc2e.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '我为伟大祖国站岗',
-    description: '',
-    lyrics: [
-      {
-        content: '手握一杆钢枪\n身披万道霞光\n我守卫在边防线上\n为我们伟大祖国站岗\n一颗红心\n时时刻刻向着北京\n站在边防线\n如同站在天安门广场\n光辉的太阳照边疆\n毛主席就在我身旁 啊\n做一个毛主席的边防战士\n无限幸福 无尚荣光\n无尚荣光\n手握一杆钢枪\n身披万道霞光\n我守卫在边防线上\n为我们伟大祖国站岗\n为我们伟大祖国站岗\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/woweiweidazuguozhangang.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '董振厚',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "我为伟大祖国站岗",
+        description: "",
+        lyrics: [
+            {
+                content: "手握一杆钢枪\n身披万道霞光\n我守卫在边防线上\n为我们伟大祖国站岗\n一颗红心\n时时刻刻向着北京\n站在边防线\n如同站在天安门广场\n光辉的太阳照边疆\n毛主席就在我身旁 啊\n做一个毛主席的边防战士\n无限幸福 无尚荣光\n无尚荣光\n手握一杆钢枪\n身披万道霞光\n我守卫在边防线上\n为我们伟大祖国站岗\n为我们伟大祖国站岗\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/woweiweidazuguozhangang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "董振厚",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我为伟大祖国站岗.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "魏宝贵",
+                ],
+            },
         ],
-        lyricists: [
-          '魏宝贵',
+        id: "760eeff5-684f-4d20-9114-86f0173ccc2e",
+        composers: [
+            "铁源",
         ],
-      },
-    ],
-    id: '760eeff5-684f-4d20-9114-86f0173ccc2e',
-    composers: [
-      '铁源',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
