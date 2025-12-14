f925aa850c569509e7074f4de23f459a53c488c5
Your Name <you@example.com>
2025-12-14 14:12:55 +0800

    1

--- 9e355a87-093b-4162-95d3-c099bdb35af8.ts
+++ 9e355a87-093b-4162-95d3-c099bdb35af8.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '9e355a87-093b-4162-95d3-c099bdb35af8',
-    name: '工作就是斗争',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl94工作就是斗争.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "9e355a87-093b-4162-95d3-c099bdb35af8",
+        name: "工作就是斗争",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl94工作就是斗争.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl94xa工作就是斗争.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
