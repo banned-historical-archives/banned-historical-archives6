b3a0a60ce9fbfc178b542e829a2a964c1f26a34f
Your Name <you@example.com>
2025-12-14 14:56:10 +0800

    1

--- 15c08412-2084-4f0b-8f6d-c5864f6976dd.ts
+++ 15c08412-2084-4f0b-8f6d-c5864f6976dd.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '为咱亲人补军装',
-    description: '',
-    lyrics: [
-      {
-        content: '井冈红旗迎风扬\n赣江滔滔把歌唱哟\n解放军野营到山村哎\n红军传统大发扬\n亲人回到根据地哟\n井冈人民心欢畅\n黄洋界上同站岗\n鄱阳湖畔同插秧哟\n爬山越岭练战术哎\n荆棘划破绿军装\n为咱亲人补军装哟\n井冈儿女喜洋洋\n针针缝进阶级爱\n线线串起情谊长哟\n军民团结一家亲哎\n为咱亲人补军装\n军民团结一家亲哟\n为咱亲人补军装\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/weizanqinrenbujunzuang.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '合唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "为咱亲人补军装",
+        description: "",
+        lyrics: [
+            {
+                content: "井冈红旗迎风扬\n赣江滔滔把歌唱哟\n解放军野营到山村哎\n红军传统大发扬\n亲人回到根据地哟\n井冈人民心欢畅\n黄洋界上同站岗\n鄱阳湖畔同插秧哟\n爬山越岭练战术哎\n荆棘划破绿军装\n为咱亲人补军装哟\n井冈儿女喜洋洋\n针针缝进阶级爱\n线线串起情谊长哟\n军民团结一家亲哎\n为咱亲人补军装\n军民团结一家亲哟\n为咱亲人补军装\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/weizanqinrenbujunzuang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/为咱亲人补军装.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "江西省歌舞团创作组",
+                ],
+            },
         ],
-        lyricists: [
-          '江西省歌舞团创作组',
+        id: "15c08412-2084-4f0b-8f6d-c5864f6976dd",
+        composers: [
+            "江西省歌舞团创作组",
         ],
-      },
-    ],
-    id: '15c08412-2084-4f0b-8f6d-c5864f6976dd',
-    composers: [
-      '江西省歌舞团创作组',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
