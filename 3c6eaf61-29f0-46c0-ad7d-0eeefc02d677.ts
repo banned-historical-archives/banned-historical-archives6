db493afcb578f2f0365a182508ee75c8d0fff5fc
Your Name <you@example.com>
2025-12-14 13:49:50 +0800

    1

--- 3c6eaf61-29f0-46c0-ad7d-0eeefc02d677.ts
+++ 3c6eaf61-29f0-46c0-ad7d-0eeefc02d677.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '3c6eaf61-29f0-46c0-ad7d-0eeefc02d677',
-    name: '我们是青年工人',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/529我们是青年工人 江西省歌舞团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '江西省歌舞团',
-                type: '合唱团',
-              },
-            ],
-            art_forms: [
-              '合唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "3c6eaf61-29f0-46c0-ad7d-0eeefc02d677",
+        name: "我们是青年工人",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/529我们是青年工人 江西省歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "江西省歌舞团",
+                                type: "合唱团",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是青年工人.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
