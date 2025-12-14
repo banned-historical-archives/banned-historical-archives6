799e13a745dbc8aba2b3fc1e24d2fb6cedb0acb0
Your Name <you@example.com>
2025-12-14 14:20:04 +0800

    1

--- fec85188-e176-49a3-a474-042a63d26801.ts
+++ fec85188-e176-49a3-a474-042a63d26801.ts
@@ -30,6 +30,14 @@ export default {
                             "独唱",
                         ],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/山乡盼着你们来1.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
                 ],
                 lyricists: [
                     "征明万",
