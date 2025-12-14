4737427aac27b9f7cc0bcaded0975c218bd2f7a7
Your Name <you@example.com>
2025-12-14 14:53:03 +0800

    1

--- ac042761-7e5c-4e1a-a053-785e7317fba9.ts
+++ ac042761-7e5c-4e1a-a053-785e7317fba9.ts
@@ -38,6 +38,14 @@ export default {
                             "独唱",
                         ],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl72a争取胜利.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
                 lyricists: [],
             },
