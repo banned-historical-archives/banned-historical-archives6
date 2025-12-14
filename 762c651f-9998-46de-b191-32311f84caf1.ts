f7a5e93cd69493509e21622247665dc6d551d80d
Your Name <you@example.com>
2025-12-14 14:55:49 +0800

    1

--- 762c651f-9998-46de-b191-32311f84caf1.ts
+++ 762c651f-9998-46de-b191-32311f84caf1.ts
@@ -1,33 +1,46 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '762c651f-9998-46de-b191-32311f84caf1',
-    name: '为巩固无产阶级专政奋勇斗争',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/671为巩固无产阶级专政奋勇斗争 铁道兵歌舞团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '铁道兵歌舞团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "762c651f-9998-46de-b191-32311f84caf1",
+        name: "为巩固无产阶级专政奋勇斗争",
+        description: "",
+        lyrics: [
+            {
+                content: "革命的洪流排山倒海\n马列主义大旗永放光彩\n全国军民团结战斗\n反修防修战歌豪迈\n为巩固无产阶级专政奋勇斗争\n看继续革命的大军豪情满怀\n党的基本路线指引我们\n奔向更加光辉灿烂的未来\n同志们前进，同志们前进\n奔向更加光辉灿烂的未来\n阳光洒满江南塞外\n毛泽东思想普照万代\n红色江山永不变色\n祖国大地春光常在\n为巩固无产阶级专政奋勇斗争\n看继续革命的大军豪情满怀\n党的基本路线指引我们\n奔向更加光辉灿烂的未来\n同志们前进，同志们前进\n奔向更加光辉灿烂的未来",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/671为巩固无产阶级专政奋勇斗争 铁道兵歌舞团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "铁道兵歌舞团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/wggwcjjzzfydz.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "解放军铁道兵歌舞团",
+                                type: "合唱团",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
