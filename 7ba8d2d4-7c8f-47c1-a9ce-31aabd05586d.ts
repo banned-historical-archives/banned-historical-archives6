09329507f63a7bedb4414b9a11fad1ca7c4857cc
Your Name <you@example.com>
2025-12-14 13:57:43 +0800

    1

--- 7ba8d2d4-7c8f-47c1-a9ce-31aabd05586d.ts
+++ 7ba8d2d4-7c8f-47c1-a9ce-31aabd05586d.ts
@@ -1,33 +1,50 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '7ba8d2d4-7c8f-47c1-a9ce-31aabd05586d',
-    name: '我为文化大革命高唱赞歌',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/621我为文化大革命高唱赞歌 管维俊.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '管维俊',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "7ba8d2d4-7c8f-47c1-a9ce-31aabd05586d",
+        name: "我为文化大革命高唱赞歌",
+        description: "",
+        lyrics: [
+            {
+                content: "大海哟 擂响了万面金鼓\n江河哟 拨动了千根银弦\n我为文化大革命高唱赞歌\n胜利成果 光辉灿烂\n毛主席亲自点燃火炬\n革命大字报烈火燎原\n毛主席亲自点燃火炬\n革命大字报烈火燎原\n各族人民高举战旗\n反帝反修 勇往直前\n啊 文化大革命\n荡涤了污泥浊水\n毛主席的革命路线\n光照河山\n阳关哟 照亮了五湖四海\n东风哟 吹绿了万水千山\n我为文化大革命高唱赞歌\n锦绣大地 春色满园\n新生事物像百花盛开\n上层建筑领域改个新观\n新生事物像百花盛开\n上层建筑领域改个新观\n一代新人朝气蓬勃\n继续革命 奋勇向前\n啊 文化大革命\n迎来了祖国春天\n毛主席的革命路线\n光照河山\n",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/621我为文化大革命高唱赞歌 管维俊.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "管维俊",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/wwwhdgmgchzg.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "管维俊",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "周祥钧",
+                ],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [
+            "程茹辛",
+        ],
+    },
 };
