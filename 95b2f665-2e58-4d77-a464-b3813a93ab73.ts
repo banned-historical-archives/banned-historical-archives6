e198bb87621f07c83b5ccba19ed4471a91b64470
Your Name <you@example.com>
2025-12-14 15:04:27 +0800

    1

--- 95b2f665-2e58-4d77-a464-b3813a93ab73.ts
+++ 95b2f665-2e58-4d77-a464-b3813a93ab73.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '万岁！伟大的中国共产党',
-    description: '',
-    lyrics: [
-      {
-        content: '\n\n伟大的中国共产党是\n伟大领袖毛主席培育的党\n无产阶级先进分子所组成\n领导我们事业的核心力量\n在伟大革命征途中乘风破浪\n毛主席的革命路线指引航向\n万岁万岁\n伟大的领袖毛主席\n万岁万岁\n伟大光荣正确的中国共产党\n\n伟大的中国共产党是\n是马列主义毛泽东思想武装的党\n朝气蓬勃  团结如钢\n反帝反修斗志昂扬\n在无产阶级专政下继续革命\n昂首阔步奔向共产主义胜利前方\n万岁万岁\n伟大的领袖毛主席\n万岁万岁\n伟大光荣正确的中国共产党\n\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/wswddzhggchd.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "万岁！伟大的中国共产党",
+        description: "",
+        lyrics: [
+            {
+                content: "\n\n伟大的中国共产党是\n伟大领袖毛主席培育的党\n无产阶级先进分子所组成\n领导我们事业的核心力量\n在伟大革命征途中乘风破浪\n毛主席的革命路线指引航向\n万岁万岁\n伟大的领袖毛主席\n万岁万岁\n伟大光荣正确的中国共产党\n\n伟大的中国共产党是\n是马列主义毛泽东思想武装的党\n朝气蓬勃  团结如钢\n反帝反修斗志昂扬\n在无产阶级专政下继续革命\n昂首阔步奔向共产主义胜利前方\n万岁万岁\n伟大的领袖毛主席\n万岁万岁\n伟大光荣正确的中国共产党\n\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/wswddzhggchd.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/万岁！伟大的中国共产党.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [
+                    "北京部队政治部宣传队北京部队政治部宣传队",
+                ],
+            },
         ],
-        lyricists: [
-          '北京部队政治部宣传队北京部队政治部宣传队',
+        id: "95b2f665-2e58-4d77-a464-b3813a93ab73",
+        composers: [
+            "北京部队政治部宣传队北京部队政治部宣传队",
         ],
-      },
-    ],
-    id: '95b2f665-2e58-4d77-a464-b3813a93ab73',
-    composers: [
-      '北京部队政治部宣传队北京部队政治部宣传队',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
