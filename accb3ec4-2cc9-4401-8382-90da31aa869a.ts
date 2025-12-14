08f0242497c74f0aa62cfe4a49651a36862b5f72
Your Name <you@example.com>
2025-12-14 14:31:55 +0800

    1

--- accb3ec4-2cc9-4401-8382-90da31aa869a.ts
+++ accb3ec4-2cc9-4401-8382-90da31aa869a.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '喜摘丰收棉',
-    description: '',
-    lyrics: [
-      {
-        content: '\n红太阳光辉照四方\n棉花吐絮闪银光\n农垦战士心欢畅\n你追我赶摘棉忙摘棉忙哟\n摘呀摘呀摘呀摘呀\n跃进歌儿唱不停\n摘呀摘呀摘呀摘呀\n满满花包送进仓\n站在棉田望北京哟\n胸怀祖国豪情壮哟\n我们喜摘丰收棉\n喜摘丰收棉哟哎嗨哎\n为建设社会主义出力量哟\n大寨红花遍地开放\n农场一片好风光好风光哟\n摘呀摘呀摘呀摘呀\n一朵棉花一颗心\n摘呀摘呀摘呀摘呀\n颗颗红心献给党\n站在棉田望北京哟\n胸怀祖国豪情壮哟\n我们喜摘丰收棉\n喜摘丰收棉哟哎嗨哎\n为建设社会主义出力量哟\n摘呀摘呀摘呀摘呀\n摘呀摘呀摘呀摘呀\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xizhaifengshoumian.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '上海市奉贤县星火农场文艺宣传队徐幼伟领唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "喜摘丰收棉",
+        description: "",
+        lyrics: [
+            {
+                content: "\n红太阳光辉照四方\n棉花吐絮闪银光\n农垦战士心欢畅\n你追我赶摘棉忙摘棉忙哟\n摘呀摘呀摘呀摘呀\n跃进歌儿唱不停\n摘呀摘呀摘呀摘呀\n满满花包送进仓\n站在棉田望北京哟\n胸怀祖国豪情壮哟\n我们喜摘丰收棉\n喜摘丰收棉哟哎嗨哎\n为建设社会主义出力量哟\n大寨红花遍地开放\n农场一片好风光好风光哟\n摘呀摘呀摘呀摘呀\n一朵棉花一颗心\n摘呀摘呀摘呀摘呀\n颗颗红心献给党\n站在棉田望北京哟\n胸怀祖国豪情壮哟\n我们喜摘丰收棉\n喜摘丰收棉哟哎嗨哎\n为建设社会主义出力量哟\n摘呀摘呀摘呀摘呀\n摘呀摘呀摘呀摘呀\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/xizhaifengshoumian.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "上海市奉贤县星火农场文艺宣传队徐幼伟领唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/喜摘丰收棉.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "上海奉贤县星火农场",
+                ],
+            },
         ],
-        lyricists: [
-          '上海奉贤县星火农场',
+        id: "accb3ec4-2cc9-4401-8382-90da31aa869a",
+        composers: [
+            "上海奉贤县星火农场",
         ],
-      },
-    ],
-    id: 'accb3ec4-2cc9-4401-8382-90da31aa869a',
-    composers: [
-      '上海奉贤县星火农场',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
