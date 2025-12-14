abc33c5c15127b52027a9201101f322d16f9c99b
Your Name <you@example.com>
2025-12-14 15:04:47 +0800

    1

--- 22fce44c-f271-4d0b-8e94-6b5ec3321e88.ts
+++ 22fce44c-f271-4d0b-8e94-6b5ec3321e88.ts
@@ -1,27 +1,48 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "22fce44c-f271-4d0b-8e94-6b5ec3321e88",
-        "name": "七亿人民七亿兵",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "22fce44c-f271-4d0b-8e94-6b5ec3321e88",
+        name: "七亿人民七亿兵",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "七亿人民七亿兵\n万里江山万里营\n提高警惕保卫祖国\n我们是攻不破的钢铁长城\n高举红旗团结紧\n一面劳动一面练兵\n只要毛主席一声令下\n消灭侵略者勇敢向前冲\n七亿人民七亿兵\n万里江山万里营\n提高警惕保卫祖国\n我们是攻不破的钢铁长城\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/七亿人民七亿兵.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/七亿人民七亿兵.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/7yirenmin7tibing.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "合唱",
+                                type: "独唱",
+                            },
+                            {
+                                name: "岳伦",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "常松",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "岳伦",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
