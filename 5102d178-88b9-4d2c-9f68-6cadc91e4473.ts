defc59ef17b006a78b689bba7cbbb30b8bd2cdd5
Your Name <you@example.com>
2025-12-14 13:53:45 +0800

    1

--- 5102d178-88b9-4d2c-9f68-6cadc91e4473.ts
+++ 5102d178-88b9-4d2c-9f68-6cadc91e4473.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "5102d178-88b9-4d2c-9f68-6cadc91e4473",
-        "name": "我们是朝气蓬勃的红小兵",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "5102d178-88b9-4d2c-9f68-6cadc91e4473",
+        name: "我们是朝气蓬勃的红小兵",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是朝气蓬勃的红小兵.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是朝气蓬勃的红小兵.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/e016我们是朝气蓬勃的红小兵 中央人民广播电台少年广播合唱团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央人民广播电台少年广播合唱团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
