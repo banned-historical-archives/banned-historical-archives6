a1a53625b7df9b76f16ae7d5297a354f55fbe089
Your Name <you@example.com>
2025-12-14 13:47:05 +0800

    1

--- 7be127a4-1e5a-4bdf-810b-6b98edc3c310.ts
+++ 7be127a4-1e5a-4bdf-810b-6b98edc3c310.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '7be127a4-1e5a-4bdf-810b-6b98edc3c310',
-    name: '我们的教育方针',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl107xa我们的教育方针.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "7be127a4-1e5a-4bdf-810b-6b98edc3c310",
+        name: "我们的教育方针",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl107xa我们的教育方针.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl107x我们的教育方针.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
