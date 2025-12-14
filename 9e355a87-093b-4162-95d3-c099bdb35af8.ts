6ecc18cca539a1e4ec16068922f41b6fced17f1b
Your Name <you@example.com>
2025-12-14 14:13:18 +0800

    1

--- 9e355a87-093b-4162-95d3-c099bdb35af8.ts
+++ 9e355a87-093b-4162-95d3-c099bdb35af8.ts
@@ -30,6 +30,14 @@ export default {
                             "独唱",
                         ],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl94x工作就是斗争.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
                 lyricists: [],
             },
