2856acbf29814ef6623165fea9a539d2a42f544b
Your Name <you@example.com>
2025-12-14 14:30:28 +0800

    1

--- b91f327f-88ac-408d-a64d-9c9a5310c7b1.ts
+++ b91f327f-88ac-408d-a64d-9c9a5310c7b1.ts
@@ -1,32 +1,45 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '坚决捍卫毛泽东思想',
-    description: '',
-    lyrics: [
-      {
-        content: '\n毛泽东思想伟大红旗\n我们高举再高举\n红旗连着我们的胜利\n红旗顶着祖国的天地\n红旗是幸福的源泉\n红旗是革命的武器\n哪个胆敢反红旗\n坚决跟他拼到底\n哪个胆敢反红旗\n坚决跟他拼到底\n坚决跟他拼到底\n毛泽东思想伟大红旗\n我们高举再高举\n七亿双眼睛擦得雪亮\n？？？？坚定不移\n坚决跟着共产党\n坚决跟着毛主席\n高举红旗向前进\n革命一定革到底\n高举红旗向前进\n革命一定革到底\n革命一定革到底\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/jianjuehanweimaozedongsixiang.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '人民解放军战友文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "坚决捍卫毛泽东思想",
+        description: "",
+        lyrics: [
+            {
+                content: "\n毛泽东思想伟大红旗\n我们高举再高举\n红旗连着我们的胜利\n红旗顶着祖国的天地\n红旗是幸福的源泉\n红旗是革命的武器\n哪个胆敢反红旗\n坚决跟他拼到底\n哪个胆敢反红旗\n坚决跟他拼到底\n坚决跟他拼到底\n毛泽东思想伟大红旗\n我们高举再高举\n七亿双眼睛擦得雪亮\n？？？？坚定不移\n坚决跟着共产党\n坚决跟着毛主席\n高举红旗向前进\n革命一定革到底\n高举红旗向前进\n革命一定革到底\n革命一定革到底\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/jianjuehanweimaozedongsixiang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "人民解放军战友文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/631坚决捍卫毛泽东思想 战友文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "战友文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    id: 'b91f327f-88ac-408d-a64d-9c9a5310c7b1',
-    composers: [],
-  },
-  version: 2,
+        id: "b91f327f-88ac-408d-a64d-9c9a5310c7b1",
+        composers: [],
+    },
+    version: 2,
 };
