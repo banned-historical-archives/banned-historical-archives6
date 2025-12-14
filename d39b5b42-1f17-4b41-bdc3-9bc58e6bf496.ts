0d0efd464a76751d10f9f52ee9e092eb446a367e
Your Name <you@example.com>
2025-12-14 14:50:00 +0800

    1

--- d39b5b42-1f17-4b41-bdc3-9bc58e6bf496.ts
+++ d39b5b42-1f17-4b41-bdc3-9bc58e6bf496.ts
@@ -1,33 +1,41 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'd39b5b42-1f17-4b41-bdc3-9bc58e6bf496',
-    name: '伟大的长城',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/589伟大的长城 中央广播文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '中央广播文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "d39b5b42-1f17-4b41-bdc3-9bc58e6bf496",
+        name: "伟大的长城",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/589伟大的长城 中央广播文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "中央广播文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/伟大的长城.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
