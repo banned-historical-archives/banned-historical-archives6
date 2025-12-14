70df37b8de93307c760b199d7c007dc9f181790b
Your Name <you@example.com>
2025-12-14 14:47:51 +0800

    1

--- 4744444a-930b-4117-87a5-c6da3025ab01.ts
+++ 4744444a-930b-4117-87a5-c6da3025ab01.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  entity: {
-    name: '侗歌向着北京唱',
-    description: '',
-    lyrics: [
-      {
-        content: '弹起琵琶哎心欢畅哎咿\n吹起芦笙响四方哎  \n侗歌向着北京唱哎\n山高水远情意长哎\n侗歌向着北京唱\n歌唱侗寨大变样\n铁牛耕田云中走\n银河流水上山岗\n哎哎  铁牛耕田云中走\n银河流水上山岗  上山岗\n侗歌向着北京唱\n歌唱侗寨新气象\n山前山后机器响\n自力更生办工厂\n哎哎  山前山后机器响\n自力更生办工厂  办工厂\n侗歌向着北京唱哎咿\n越唱心里越亮堂哎\n唱不完侗家幸福歌哎\n唱不尽毛主席恩情长哎\n唱不尽毛主席恩情长哎\n',
-        version: '原版',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/donggexiangzebeijingcang.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '赵海兰领唱',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    entity: {
+        name: "侗歌向着北京唱",
+        description: "",
+        lyrics: [
+            {
+                content: "弹起琵琶哎心欢畅哎咿\n吹起芦笙响四方哎  \n侗歌向着北京唱哎\n山高水远情意长哎\n侗歌向着北京唱\n歌唱侗寨大变样\n铁牛耕田云中走\n银河流水上山岗\n哎哎  铁牛耕田云中走\n银河流水上山岗  上山岗\n侗歌向着北京唱\n歌唱侗寨新气象\n山前山后机器响\n自力更生办工厂\n哎哎  山前山后机器响\n自力更生办工厂  办工厂\n侗歌向着北京唱哎咿\n越唱心里越亮堂哎\n唱不完侗家幸福歌哎\n唱不尽毛主席恩情长哎\n唱不尽毛主席恩情长哎\n",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/donggexiangzebeijingcang.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "赵海兰领唱",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/侗歌向着北京唱.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [
+                    "龙燕怡",
+                ],
+            },
         ],
-        lyricists: [
-          '龙燕怡',
+        id: "4744444a-930b-4117-87a5-c6da3025ab01",
+        composers: [
+            "怀化",
         ],
-      },
-    ],
-    id: '4744444a-930b-4117-87a5-c6da3025ab01',
-    composers: [
-      '怀化',
-    ],
-  },
-  version: 2,
+    },
+    version: 2,
 };
