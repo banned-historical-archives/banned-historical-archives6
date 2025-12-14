4e69c14b4ce149a5abe89118650be8e7e726baa9
Your Name <you@example.com>
2025-12-14 14:47:04 +0800

    1

--- b350f552-6657-4fe7-a88b-229214a9dfb7.ts
+++ b350f552-6657-4fe7-a88b-229214a9dfb7.ts
@@ -1,27 +1,52 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "b350f552-6657-4fe7-a88b-229214a9dfb7",
-        "name": "党是春雨我是苗（非原唱）",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "b350f552-6657-4fe7-a88b-229214a9dfb7",
+        name: "党是春雨我是苗",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "公社的山水美如画嘞\n广阔天地安新家嘞\n春天栽排杨柳树啊嘞\n枝叶青青抽新芽嘞\n党是春雨我是苗\n我学杨柳把根扎嘞\n贫下中农勤浇灌嘞\n阳光雨露心头洒嘞\n一辈子扎根在农村\n革命青春放光华嘞\n\n公社的山水美如画嘞\n广阔天地安新家嘞\n山岗栽上松柏树啊嘞\n根深叶茂多挺拔嘞\n党是春雨我是苗\n我学松柏立山崖嘞\n贫下中农勤浇灌嘞\n笑迎冰霜斗风沙嘞\n一辈子扎根在农村\n革命青春放光华嘞\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/党是春雨我是苗（非原唱）.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/党是春雨我是苗（非原唱）.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/dangshichunyuwoshimiao.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/党是春雨我是苗.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [
+                    "葛军","胡细生",
+                ],
+            },
         ],
-        "composers": []
+        composers: [
+            "贺大行",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
