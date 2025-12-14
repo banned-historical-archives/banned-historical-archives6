00d0f2ccfe80c5aff8224181da435d62230719c3
Your Name <you@example.com>
2025-12-14 13:47:57 +0800

    1

--- 57e20a8d-5eae-4dc6-ae6b-2e1d121c942f.ts
+++ 57e20a8d-5eae-4dc6-ae6b-2e1d121c942f.ts
@@ -1,37 +1,45 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '57e20a8d-5eae-4dc6-ae6b-2e1d121c942f',
-    name: '我们的公社欣欣向荣',
-    description: '',
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/579我们的公社欣欣向荣 内蒙古自治区歌舞团 广播文工团.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '内蒙古自治区歌舞团',
-                type: '独唱',
-              },
-              {
-                name: '广播文工团',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "57e20a8d-5eae-4dc6-ae6b-2e1d121c942f",
+        name: "我们的公社欣欣向荣",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/579我们的公社欣欣向荣 内蒙古自治区歌舞团 广播文工团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "内蒙古自治区歌舞团",
+                                type: "独唱",
+                            },
+                            {
+                                name: "广播文工团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们的公社欣欣向荣.mp3",
+                        sources: [
+                            "战地新歌",
+                        ],
+                        artists: [],
+                        art_forms: [],
+                    },
+                ],
+                lyricists: [],
+            },
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        composers: [],
+    },
 };
