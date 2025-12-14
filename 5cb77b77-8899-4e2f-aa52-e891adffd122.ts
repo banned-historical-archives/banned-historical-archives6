e0cb4e0f85aba8c6f9a5b4ac50ff6db80cce4acc
Your Name <you@example.com>
2025-12-14 14:10:12 +0800

    1

--- 5cb77b77-8899-4e2f-aa52-e891adffd122.ts
+++ 5cb77b77-8899-4e2f-aa52-e891adffd122.ts
@@ -1,36 +1,44 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: '5cb77b77-8899-4e2f-aa52-e891adffd122',
-    name: '希望寄托在你们身上',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl70y希望寄托在你们身上 英语.mp3',
-            sources: [],
-            artists: [
-              {
-                name: '英语',
-                type: '独唱',
-              },
-            ],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "5cb77b77-8899-4e2f-aa52-e891adffd122",
+        name: "希望寄托在你们身上",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl70y希望寄托在你们身上 英语.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "英语",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl70xa希望寄托在你们身上.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
