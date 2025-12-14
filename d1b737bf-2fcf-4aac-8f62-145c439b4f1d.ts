1ca2813592e389080c79071206b3155c788a0aad
Your Name <you@example.com>
2025-12-14 14:34:49 +0800

    1

--- d1b737bf-2fcf-4aac-8f62-145c439b4f1d.ts
+++ d1b737bf-2fcf-4aac-8f62-145c439b4f1d.ts
@@ -1,31 +1,39 @@
 export default {
-  resource_type: 'music',
-  version: 2,
-  entity: {
-    id: 'd1b737bf-2fcf-4aac-8f62-145c439b4f1d',
-    name: '吃苦在别人前头，享受在别人后头',
-    description: '',
-    tags: [
-      '语录歌',
-    ],
-    lyrics: [
-      {
-        content: '',
-        version: '原版',
-        lyrictist: '',
-        audios: [
-          {
-            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl45a吃苦在别人前头，享受在别人后头.mp3',
-            sources: [],
-            artists: [],
-            art_forms: [
-              '独唱',
-            ],
-          },
+    resource_type: "music",
+    version: 2,
+    entity: {
+        id: "d1b737bf-2fcf-4aac-8f62-145c439b4f1d",
+        name: "吃苦在别人前头，享受在别人后头",
+        description: "",
+        tags: [
+            "语录歌",
         ],
-        lyricists: [],
-      },
-    ],
-    composers: [],
-  },
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                lyrictist: "",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl45a吃苦在别人前头，享受在别人后头.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/毛主席语录歌曲/myl45吃苦在别人前头，享受在别人后头.mp3",
+                        sources: [],
+                        artists: [],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        composers: [],
+    },
 };
