758af476c8db3c1e99718b85611ae9099775ab9e
Your Name <you@example.com>
2025-12-14 13:51:00 +0800

    1

--- 528e1959-7128-4213-8c2a-b6c44ac176d7.ts
+++ 528e1959-7128-4213-8c2a-b6c44ac176d7.ts
@@ -1,27 +1,40 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "528e1959-7128-4213-8c2a-b6c44ac176d7",
-        "name": "我们是红小兵",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "528e1959-7128-4213-8c2a-b6c44ac176d7",
+        name: "我们是红小兵",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是红小兵.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/我们是红小兵.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/e209我们是红小兵 北京市少年宫合唱团.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "北京市少年宫合唱团",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
