b141e57219b5ef37c80d423dba69cf677ff7c806
Your Name <you@example.com>
2025-12-14 15:02:03 +0800

    1

--- c4f93dbd-04f7-4d1c-97b2-689577c88080.ts
+++ c4f93dbd-04f7-4d1c-97b2-689577c88080.ts
@@ -1,27 +1,44 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "c4f93dbd-04f7-4d1c-97b2-689577c88080",
-        "name": "三湾来了毛委员",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "c4f93dbd-04f7-4d1c-97b2-689577c88080",
+        name: "三湾来了毛委员",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "太阳照山川哎\n红旗迎风展哎\n战歌声声军号响哎\n三湾村里亮了天哎\n一九那个二七年\n来了救星毛委员\n带来工农革命军\n三湾改编换新颜\n党支部建在连队上\n部队有了生命线\n三湾民主威力大哎\n官兵团结斗志坚\n咳  一九那个二七年\n来了救星毛委员\n带来工农革命军\n三湾改编换新颜\n创建人民新军队\n工农翻身有靠山\n毛委员指引胜利路哎\n军民携手奔向前\n创建人民新军队\n工农翻身有靠山\n毛委员指引胜利路哎\n军民携手奔向前\n奔向前哎\n",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/三湾来了毛委员.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/三湾来了毛委员.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/3wanlailemaoweiyuan.mp3",
+                        sources: [],
+                        artists: [
+                            {
+                                name: "连小凤",
+                                type: "独唱",
+                            },
+                            {
+                                name: "江西省文化组整理改编",
+                                type: "独唱",
+                            },
+                        ],
+                        art_forms: [
+                            "独唱",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [],
     },
-    "version": 2
-}
+    version: 2,
+};
