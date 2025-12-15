8dd179f833b8ce4809830189aa0a55bc654c4c89
Your Name <you@example.com>
2025-12-15 16:48:43 +0800

    11

---
+++ 30c5d42b-eaaa-4ddd-a6ab-6620a2680b86.ts
@@ -0,0 +1,42 @@
+export default {
+    resource_type: "music",
+    entity: {
+        name: "党领导我们胜利前进",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/m2018/8.mp3",
+                        sources: [
+                            "中国唱片m2018",
+                        ],
+                        artists: [
+                            {
+                                name: "中国人民解放军歌舞团合唱队",
+                                type: "演唱",
+                            },
+                            {
+                                name: "王碧云",
+                                type: "手风琴伴奏",
+                            },
+                            {
+                                name: "王振民",
+                                type: "指挥",
+                            },
+                        ],
+                        art_forms: [
+                            "男声小合唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        id: "30c5d42b-eaaa-4ddd-a6ab-6620a2680b86",
+        composers: [],
+    },
+    version: 2,
+};
---
+++ c4899c45-9459-41ef-a1d5-9d4269e4c90a.ts
@@ -0,0 +1,42 @@
+export default {
+    resource_type: "music",
+    entity: {
+        name: "党的十大光辉灿烂",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/m2018/2.mp3",
+                        sources: [
+                            "中国唱片m2018",
+                        ],
+                        artists: [
+                            {
+                                name: "江苏省文工团",
+                                type: "演唱",
+                            },
+                            {
+                                name: "南京艺术学院",
+                                type: "演唱",
+                            },
+                            {
+                                name: "石中光",
+                                type: "指挥",
+                            },
+                        ],
+                        art_forms: [
+                            "合唱",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        id: "c4899c45-9459-41ef-a1d5-9d4269e4c90a",
+        composers: [],
+    },
+    version: 2,
+};
