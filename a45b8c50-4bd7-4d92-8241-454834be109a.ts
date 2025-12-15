31a6413f948b8cb26c7d518a3f6f6dfc2c5e3490
Your Name <you@example.com>
2025-12-16 02:29:17 +0800

    1

--- a45b8c50-4bd7-4d92-8241-454834be109a.ts
+++ a45b8c50-4bd7-4d92-8241-454834be109a.ts
@@ -1,27 +1,53 @@
 export default {
-    "resource_type": "music",
-    "entity": {
-        "id": "a45b8c50-4bd7-4d92-8241-454834be109a",
-        "name": "龙腾虎跃争上游",
-        "description": "",
-        "lyrics": [
+    resource_type: "music",
+    entity: {
+        id: "a45b8c50-4bd7-4d92-8241-454834be109a",
+        name: "龙腾虎跃争上游",
+        description: "",
+        lyrics: [
             {
-                "content": "",
-                "version": "原版",
-                "audios": [
+                content: "",
+                version: "原版",
+                audios: [
                     {
-                        "url": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/龙腾虎跃争上游.mp3",
-                        "sources": [
-                            "战地新歌"
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/龙腾虎跃争上游.mp3",
+                        sources: [
+                            "战地新歌",
                         ],
-                        "artists": [],
-                        "art_forms": []
-                    }
+                        artists: [],
+                        art_forms: [],
+                    },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/m2042/8.mp3",
+                        sources: [
+                            "中国唱片m2042",
+                        ],
+                        artists: [
+                            {
+                                name: "中国人民解放军军乐团",
+                                type: "演奏",
+                            },
+                            {
+                                name: "程义明",
+                                type: "指挥",
+                            },
+                            {
+                                name: "刘玉宝",
+                                type: "指挥",
+                            },
+                        ],
+                        art_forms: [
+                            "铜管乐",
+                        ],
+                    },
                 ],
-                "lyricists": []
-            }
+                lyricists: [],
+            },
         ],
-        "composers": []
+        composers: [
+            "栾仁杰",
+            "林育",
+        ],
     },
-    "version": 2
-}
+    version: 2,
+};
