1584cb630cd1a8e2a98f8d6311ac9ab196fb5293
Your Name <you@example.com>
2025-12-16 02:31:13 +0800

    1

--- a45b8c50-4bd7-4d92-8241-454834be109a.ts
+++ a45b8c50-4bd7-4d92-8241-454834be109a.ts
@@ -40,6 +40,29 @@ export default {
                             "铜管乐",
                         ],
                     },
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/m2041/2.mp3",
+                        sources: [
+                            "中国唱片m2041",
+                        ],
+                        artists: [
+                            {
+                                name: "石中光",
+                                type: "指挥",
+                            },
+                            {
+                                name: "江苏省文工团",
+                                type: "演唱",
+                            },
+                            {
+                                name: "南京艺术学院",
+                                type: "演唱",
+                            },
+                        ],
+                        art_forms: [
+                            "齐唱",
+                        ],
+                    },
                 ],
                 lyricists: [],
             },
