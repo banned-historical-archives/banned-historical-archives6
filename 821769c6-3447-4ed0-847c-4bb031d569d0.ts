99b20cf8959614c9cdd1412e8c3988e574c1a05c
Your Name <you@example.com>
2025-12-15 21:55:51 +0800

    1

---
+++ 821769c6-3447-4ed0-847c-4bb031d569d0.ts
@@ -0,0 +1,32 @@
+export default {
+    resource_type: "music",
+    entity: {
+        name: "沿着社会主义大道奔前方",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/L0003/3.mp3",
+                        sources: [
+                            "中国唱片L0003",
+                        ],
+                        artists: [],
+                        art_forms: [
+                            "合奏",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        id: "821769c6-3447-4ed0-847c-4bb031d569d0",
+        composers: [
+            "施万春",
+            "彭修文",
+        ],
+    },
+    version: 2,
+};
---
+++ a42704b5-17a1-4307-b899-b37eeeac788a.ts
@@ -0,0 +1,37 @@
+export default {
+    resource_type: "music",
+    entity: {
+        name: "草原上的红卫兵见到了毛主席",
+        description: "",
+        lyrics: [
+            {
+                content: "",
+                version: "原版",
+                audios: [
+                    {
+                        url: "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/L0003/1.mp3",
+                        sources: [
+                            "中国唱片L0003",
+                        ],
+                        artists: [
+                            {
+                                name: "彭修文",
+                                type: "指挥",
+                            },
+                        ],
+                        art_forms: [
+                            "合奏",
+                        ],
+                    },
+                ],
+                lyricists: [],
+            },
+        ],
+        id: "a42704b5-17a1-4307-b899-b37eeeac788a",
+        composers: [
+            "高士衡",
+            "李德保",
+        ],
+    },
+    version: 2,
+};
