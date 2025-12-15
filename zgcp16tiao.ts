e5373ccaf7f56f2840455c60b18105b4f2a0cc06
Your Name <you@example.com>
2025-12-15 18:03:54 +0800

    16

---
+++ zgcp16tiao.ts
@@ -0,0 +1,29 @@
+export default {
+  resource_type: 'music',
+  version: 2,
+  entity: {
+    id: 'zgcp16tiao',
+    name: '中国共产党中央委员会关于无产阶级文化大革命的决定',
+    description: '',
+    lyrics: [
+      {
+        content: '',
+        version: '原版',
+        lyrictist: '',
+        audios: [
+          {
+            url: 'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives6/main/16tiao/1.mp3',
+            sources: ['中国唱片9-0005', '中国唱片9-0006', '中国唱片9-0007', '中国唱片9-0008', '中国唱片9-0009'],
+            artists: [
+            ],
+            art_forms: [
+              '录音',
+            ],
+          },
+        ],
+        lyricists: ['毛泽东'],
+      },
+    ],
+    composers: [],
+  },
+};
